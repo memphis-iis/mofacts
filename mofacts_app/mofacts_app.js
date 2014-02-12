2d7ad8e2326d7101bbe45aa99c4086c4e21bd2d6
Ian Burns <iwburns8@gmail.com>
2014-02-12 12:53:30 -0600

    Changed columns to adapt to extra small viewports.  Also added button click detection for both the "sign in" and "sign up" buttons.

--- mofacts_app/mofacts_app.js
+++ mofacts_app/mofacts_app.js
@@ -1,21 +1,34 @@
 
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to mofacts_app.";
-  };
+    Template.hello.greeting = function () {
+        return "Welcome to mofacts_app.";
+    };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+    Template.hello.events({
+        'click input' : function () {
+            // template data, if any, is available in 'this'
+            if (typeof console !== 'undefined')
+                console.log("You pressed the button");
+        }
+    });
+
+    Template.login.events({
+        'click #signInButton' : function () {
+            if (typeof console !== 'undefined') {
+                console.log("You are trying to sign in!");
+            }
+        },
+        'click #signUpButton' : function () {
+            if (typeof console !== 'undefined') {
+                console.log("You are trying to sign up!");
+            }
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
