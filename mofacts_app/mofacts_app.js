d237363c189f5dbc3c68ed512d7f27fbf90f4e04
Ian Burns <iwburns8@gmail.com>
2014-02-12 21:49:08 -0600

    Added a handlebars helper called "equals" for use with this {{#if }} handlebar. Attempted to make the pages link to each other by changing the session variable "currentTemplate".

--- mofacts_app/mofacts_app.js
+++ mofacts_app/mofacts_app.js
@@ -1,8 +1,18 @@
-
-
 if (Meteor.isClient) {
 
+    Session.setDefault('currentTemplate', 'signInTemplate');
+    
+    Template.baseTemplate.currentTemplate = Session.get('currentTemplate');
+    Template.baseTemplate.signInTemplate = 'signInTemplate';
+    Template.baseTemplate.signUpTemplate = 'signUpTemplate';
 
+    Handlebars.registerHelper('equals', function (v1, v2, options) {
+        if (v1 === v2) {
+            return true;
+        } else {
+            return false;
+        }
+    });
 
     Template.hello.greeting = function () {
         return "Welcome to mofacts_app.";
@@ -21,15 +31,23 @@ if (Meteor.isClient) {
             if (typeof console !== 'undefined') {
                 console.log("You are trying to sign in!");
             }
+            //sign the user in here.
         },
         'click #signUpButton' : function () {
             if (typeof console !== 'undefined') {
                 console.log("You are trying to sign up!");
             }
+            Session.set('currentTemplate', 'signUpTemplate');
         }
     });
 
     Template.signUpTemplate.events({
+        'click #signInButton' : function () {
+            if (typeof console !== 'undefined') {
+                console.log("You are trying to sign in!");
+            }
+            Session.set('currentTemplate', 'signInTemplate');
+        },
         'click #createAccountButton' : function () {
             //create the new user account here.
         }
