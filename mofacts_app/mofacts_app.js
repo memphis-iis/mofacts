e84040c3286cb6b5a4b068e54b545d32d90992fd
Ian Burns <iwburns8@gmail.com>
2014-02-14 14:13:00 -0600

    Added some comments in the .js file.  Also switched back to the baseTemplate display.  Still can't get the template to change to a different page.

--- mofacts_app/mofacts_app.js
+++ mofacts_app/mofacts_app.js
@@ -31,13 +31,15 @@ if (Meteor.isClient) {
             if (typeof console !== 'undefined') {
                 console.log("You are trying to sign in!");
             }
-            //sign the user in here.
+            var newUsername = username.value;
+            var newPassword = password.value;
+            //IWB 2/14/2014 - sign the user in here.
         },
         'click #signUpButton' : function () {
             if (typeof console !== 'undefined') {
                 console.log("You are trying to sign up!");
             }
-            //switch the template to the signUpTemplate.
+            //IWB 2/14/2014 - switch the template to the signUpTemplate.
         }
     });
 
@@ -46,23 +48,40 @@ if (Meteor.isClient) {
             if (typeof console !== 'undefined') {
                 console.log("You are trying to sign in!");
             }
-            Session.set('currentTemplate', 'signInTemplate');
+            //IWB 2/14/2014 - switch the template to the signInTemplate.
         },
         'click #createAccountButton' : function () {
             var newUsername = username.value;
             var newPassword1 = password1.value;
             var newPassword2 = password2.value;
+
+            if (newUsername === "") {
+                //IWB 2/14/2014 - we need to let them know that their username has certain
+                //requirements (which will have to be changed above).
+                //for now we can just log it, but later we will need to put this on the page.
+                console.log("Username must be at least 6 characters long.");
+                return;
+            }
             
-            //we need to check if the newUsername is already in use.
+            //IWB 2/14/2014 - we need to check if the newUsername is already in use.
             //this requires that the DB already exists.
 
             if (newPassword1 !== newPassword2) {
-                //we need to let them know that their passwords must be the same.
+                //IWB 2/14/2014 - we need to let them know that their passwords must be the same.
+                //for now we can just log it, but later we will need to put this on the page.
                 console.log("Please make sure the passwords you typed in are the same.");
                 return;
             }
 
-            //once the above checks have been completed, we can add the user to the DB.
+            if (newPassword1 === "") {
+                //IWB 2/14/2014 - we need to let them know that their password has certain
+                //requirements (which will have to be changed above).
+                //for now we can just log it, but later we will need to put this on the page.
+                console.log("Please make sure the passwords you typed in are the same.");
+                return;
+            }
+
+            //IWB 2/14/2014 - once the above checks have been completed, we can add the user to the DB.
 
         }
     });
