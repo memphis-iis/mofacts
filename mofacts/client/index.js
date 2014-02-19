52c858ceaa6819def48c342e1eaafa147e513c15
Ian Burns <iwburns8@gmail.com>
2014-02-19 15:11:51 -0600

    Added Iron-router package under packages.

--- mofacts/client/index.js
+++ mofacts/client/index.js
@@ -2,7 +2,7 @@ Meteor.startup(function() {
 	Meteor.call("getStimuli",
 		function (err, result) {
 			Session.setDefault('stimuli', result);
-			//console.log(Session.get('stimuli').setspec.clusters[0].cluster[0].word[0]);
+			console.log(Session.get('stimuli').setspec.clusters[0].cluster[0].word[0]);
 		}
 	);
 });
