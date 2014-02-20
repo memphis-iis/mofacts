2e2a186c96c7bef639812710808936e7ccc5c3ee
Ian Burns <iwburns8@gmail.com>
2014-02-19 21:02:27 -0600

    Changed the view code for both sign in and sign up.  They should now be more like the mock-ups. Also added some live error detection on the sign in and sign up pages.

--- mofacts/client/views/signInTemplate.js
+++ mofacts/client/views/signInTemplate.js
@@ -3,22 +3,28 @@ Template.signInTemplate.events({
         if (typeof console !== 'undefined') {
             console.log("You are trying to sign in!");
         }
-        var newUsername = username.value;
+        var newUsername = signInUsername.value;
         var newPassword = password.value;
         
         Meteor.loginWithPassword(newUsername, newPassword, function(error) {
             if (typeof error !== 'undefined') {
-                console.log(error);
+                // console.log(error);
+                $("#invalidLogin").show();
+                return;
             } else {
+                $("#invalidLogin").hide();
                 var currentUser = Meteor.users.findOne({_id: Meteor.userId()}).username;
                 console.log(currentUser + " was logged in successfully!");
             }
         });
-
-        
-
     },
     'click #signUpButton' : function () {
 		Router.go("signup");
+    },
+    'focus #signInUsername' : function () {
+        $("#invalidLogin").hide();
+    },
+    'focus #password' : function () {
+        $("#invalidLogin").hide();
     }
 });
--- mofacts/client/views/signUpTemplate.js
+++ mofacts/client/views/signUpTemplate.js
@@ -1,45 +1,76 @@
 Template.signUpTemplate.events({
     'click #signInButton' : function () {
 		Router.go('signin');
-        //IWB 2/14/2014 - switch the template to the signInTemplate.
     },
     'click #signUpButton' : function () {
-        var newUsername = username.value;
+        var newUsername = signUpUsername.value;
         var newPassword1 = password1.value;
         var newPassword2 = password2.value;
 
-        if (newUsername === "") {
-            $(".badUsername").show();
-            //IWB 2/14/2014 - we need to let them know that their username has certain
-            //requirements (which will have to be changed above).
-            //for now we can just log it, but later we will need to put this on the page.
-            console.log("Username must be at least 6 characters long.");
+        if(newUsername.length < 6) {
+            $("#usernameTooShort").show();
             return;
+        } else {
+            $("#usernameTooShort").hide();
         }
         
-        //IWB 2/14/2014 - we need to check if the newUsername is already in use.
-        //this requires that the DB already exists.
-
-        if (newPassword1 !== newPassword2) {
-            //IWB 2/14/2014 - we need to let them know that their passwords must be the same.
-            //for now we can just log it, but later we will need to put this on the page.
-            console.log("Please make sure the passwords you typed in are the same.");
+        var userWithGivenUsername = Meteor.users.findOne({username: newUsername});
+        if(typeof userWithGivenUsername !== "undefined") {
+            $("#usernameAlreadyInUse").show();
             return;
+        } else {
+            $("#usernameAlreadyInUse").hide();
         }
 
-        if (newPassword1 === "") {
-            //IWB 2/14/2014 - we need to let them know that their password has certain
-            //requirements (which will have to be changed above).
-            //for now we can just log it, but later we will need to put this on the page.
-            console.log("Please type in a password in both password fields.");
-            return;
+        if (newPassword1.length < 6) {
+             $("#passwordTooShort").show();
+             return;
+        } else {
+            $("#passwordTooShort").hide();
         }
 
-        //IWB 2/14/2014 - once the above checks have been completed, we can add the user to the DB.
+        if(newPassword1 !== newPassword2) {
+            $("#passwordMustMatch").show();
+            return;
+        } else {
+            $("#passwordMustMatch").hide();
+        }
+
         Accounts.createUser({username: newUsername, password: newPassword1});
 
-        var currentUser = Meteor.users.findOne({_id: Meteor.userId()}).username;
-        console.log( currentUser + " is logged in!");
+        if (Meteor.user() !== null) {
+            console.log( Meteor.user().username + " is logged in!");
+        } else {
+            //there was an issue with the account creation or the login thereafter.
+        }
+        Meteor.logout();
+    },
+    'blur #signUpUsername' : function () {
+        if(signUpUsername.value.length < 6) {
+            $("#usernameTooShort").show();
+        } else {
+            $("#usernameTooShort").hide();
+        }
 
+        var userWithGivenUsername = Meteor.users.findOne({username: signUpUsername.value});
+        if(typeof userWithGivenUsername !== "undefined") {
+            $("#usernameAlreadyInUse").show();
+        } else {
+            $("#usernameAlreadyInUse").hide();
+        }
+    },
+    'blur #password1' : function () {
+        if(password1.value.length < 6) {
+            $("#passwordTooShort").show();
+        } else {
+            $("#passwordTooShort").hide();
+        }
+    },
+    'blur #password2' : function () {
+        if(password1.value !== password2.value) {
+            $("#passwordMustMatch").show();
+        } else {
+            $("#passwordMustMatch").hide();
+        }
     }
 });
