a9469ab71a18c6e155ba36e6816c234d6f7a88c3
Andrew DiMotta <adimotta@memphis.edu>
2014-02-24 15:06:34 -0600

    Initialized Stimuli

--- mofacts/client/views/cardTemplate.js
+++ mofacts/client/views/cardTemplate.js
@@ -1,3 +1,6 @@
 Template.cardTemplate.invokeAfterLoad = function() {
 	console.log('card loaded');
+	Session.setDefault("currentQuestion", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[Math.floor((Math.random() * 17))].word[0]);
+	Session.setDefault("currentAnswer", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[Math.floor((Math.random() * 17))].answer[0]);
+	
 }
