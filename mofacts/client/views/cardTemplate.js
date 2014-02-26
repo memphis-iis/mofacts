e5ce9390ef3d28666a902b7b803fb9082e8e8525
Andrew DiMotta <adimotta@memphis.edu>
2014-02-26 15:35:34 -0600

    created randomCard() function

--- mofacts/client/views/cardTemplate.js
+++ mofacts/client/views/cardTemplate.js
@@ -1,14 +1,18 @@
 Template.cardTemplate.invokeAfterLoad = function() {
 	console.log('card loaded');
+	randomCard();
+}
+
+function randomCard() {
 	Session.setDefault("currentQuestion", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[Math.floor((Math.random() * 17))].word[0]);
 	Session.setDefault("currentAnswer", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[Math.floor((Math.random() * 17))].answer[0]);
-	
-	function handleKeyPress(e){
-		 var key=e.keyCode || e.which;
-		  if (key==13){
-			  //searching();
-			  console.log('You Clicked "Enter"');
+
+}
+
+function handleKeyPress(e){
+    var key=e.keyCode || e.which;
+		 if (key==13){
+			 //searching();
+			 console.log('You Clicked "Enter"');
 		  }
 		}
-	
-}
