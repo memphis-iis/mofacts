6e7d1e4b386dc22b47257291d07e3bf465cf2898
Ian Burns <iwburns8@gmail.com>
2014-02-27 22:19:40 -0600

    Did some formatting on my js files.  Now everything is in it's own section with a label and all files are organized the same way.

--- mofacts/client/views/profileTemplate.js
+++ mofacts/client/views/profileTemplate.js
@@ -1,5 +1,35 @@
+//////////////
+//  EVENTS  //
+//////////////
+
+Template.profileTemplate.events({
+    'click .logoutLink' : function () {
+        Meteor.logout( function (error) {
+            if (typeof error !== "undefined") {
+                //something happened during logout
+                console.log("Error: Could not sign out of account! \n" +
+                            "\tUser: " + Meteor.user() +" \n");
+            } else {
+                Router.go("signin");
+            }
+        });
+    },
+    'click .homeLink' : function () {
+        Router.go("profile");
+    },
+    'click .stimButton' : function (event) {
+        Session.set("currentTest", event.target.name);
+        WritetoFile(event.target.name);
+        console.log("You clicked on: " + Session.get("currentTest"));
+        Router.go("card");
+    }
+});
+
+/////////////////
+//  VARIABLES  //
+/////////////////
+
 Template.profileTemplate.rendered = function () {
-    
 	//this is called whenever the template is rendered.
     var allStimuli = Stimuli.find({});
     allStimuli.forEach( function (stimuliObject) {
@@ -14,20 +44,6 @@ Template.profileTemplate.rendered = function () {
     });
 }
 
-function WritetoFile(content){
-	console.log("You clicked on: " + content);
-	
-	//var fs = require('fs');
-	
-	//fs.writeFile("/tmp/test", content, function(err) {
-	//    if(err) {
-	//        console.log(err);
-	//    } else {
-	//        console.log("The file was saved!");
-	//    }
-	//}); 
-}
-
 Template.profileTemplate.username = function () {
     if (typeof Meteor.users.findOne({_id: Meteor.userId()}) === "undefined" ) {
         Router.go("signin");
@@ -37,26 +53,20 @@ Template.profileTemplate.username = function () {
 	return Meteor.users.findOne({_id: Meteor.userId()}).username;
 }
 
-Template.profileTemplate.events({
-    'click .logoutLink' : function () {
-    	Meteor.logout( function (error) {
-    		if (typeof error !== "undefined") {
-    			//something happened during logout
-    			console.log("Error: Could not sign out of account! \n" +
-    						"\tUser: " + Meteor.user() +" \n");
-    		} else {
-    			Router.go("signin");
-    		}
-    	});
-    },
-    'click .homeLink' : function () {
-    	Router.go("profile");
-    },
-    'click .stimButton' : function (event) {
-        Session.set("currentTest", event.target.name);
-        WritetoFile(event.target.name);
-        console.log("You clicked on: " + Session.get("currentTest"));
-        Router.go("card");
-    }
+/////////////////
+//  FUNCTIONS  //
+/////////////////
 
-});
+function WritetoFile(content){
+    console.log("You clicked on: " + content);
+    
+    //var fs = require('fs');
+    
+    //fs.writeFile("/tmp/test", content, function(err) {
+    //    if(err) {
+    //        console.log(err);
+    //    } else {
+    //        console.log("The file was saved!");
+    //    }
+    //}); 
+}
