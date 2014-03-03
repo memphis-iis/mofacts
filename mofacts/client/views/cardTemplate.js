4c0bcfec2819ebf4637542390ae6b328a0d4b2d9
Ian Burns <iwburns8@gmail.com>
2014-03-02 19:42:06 -0600

    Added a variable to store the random number in cardTemplate.js.  This should fix the bug where questions did not match answers.

--- mofacts/client/views/cardTemplate.js
+++ mofacts/client/views/cardTemplate.js
@@ -4,9 +4,9 @@ Template.cardTemplate.invokeAfterLoad = function() {
 }
 
 function randomCard() {
-	Session.setDefault("currentQuestion", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[Math.floor((Math.random() * 17))].word[0]);
-	Session.setDefault("currentAnswer", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[Math.floor((Math.random() * 17))].answer[0]);
-
+	var nextCardIndex = Math.floor((Math.random() * 17));
+	Session.setDefault("currentQuestion", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[nextCardIndex].word[0]);
+	Session.setDefault("currentAnswer", Stimuli.findOne({fileName: "EEGstims.xml"}).stimuli.setspec.clusters[0].cluster[nextCardIndex].answer[0]);
 }
 
 Template.cardTemplate.events({
