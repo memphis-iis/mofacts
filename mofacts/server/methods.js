6f3427c28a149581c1722c707174c962513cf130
James <jmantoin@memphis.edu>
2014-03-04 05:55:44 -0600

    Moved stims into a subfolder

--- mofacts/server/methods.js
+++ mofacts/server/methods.js
@@ -3,15 +3,14 @@ var fs = Npm.require("fs");
 
 Meteor.startup(function () {
 	Stimuli.remove({});
-	var files = fs.readdirSync('./assets/app/');
-
+	var files = fs.readdirSync('./assets/app/stims/');
 	var stims = _(files).reject( function(fileName) {
 		return fileName.indexOf('.xml') < 0;
 	});
 
 	for(var i = 0; i < stims.length; i++){
 		var fileName = stims[i];
-		var json = getStimJSON(fileName);
+		var json = getStimJSON('stims/' + fileName);
 		Stimuli.insert({fileName: fileName, stimuli: json});
 	}
 });
