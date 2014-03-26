510d3e884b7410928f0fb899c517881c22aaaffa
paperziggurat <paperziggurat@gmail.com>
2014-03-26 14:36:32 -0500

    Tdfs collection defined

--- mofacts/common/Collections.js
+++ mofacts/common/Collections.js
@@ -1,3 +1,5 @@
 Stimuli = new Meteor.Collection("stimuli");
+Tdfs = new Meteor.Collection("tdfs");
+stimTdfPair = new Meteor.Collection("tdfValue");
 Paused = new Meteor.Collection("paused");
 UserProgress = new Meteor.Collection("userProgress");
