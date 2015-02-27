e30008c8a097bc6bc4bbd0f8a2e7cbf032abe383
Craig Kelly <craig.n.kelly@gmail.com>
2015-02-27 17:33:14 -0600

    TODO comment tweak

--- mofacts/client/views/adminTemplate.js
+++ mofacts/client/views/adminTemplate.js
@@ -1,8 +1,10 @@
+//TODO: download experiment data from TDF list selection (using experiment_times.js)
+
 Template.adminTemplate.rendered = function() {
 };
 
 Template.adminTemplate.helpers({
    results: function() {
-       return Meteor.user();  //TODO: everything (anything) in this template
+       return Meteor.user();
    }
 });
