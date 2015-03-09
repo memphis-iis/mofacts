00bd0b08b2253ecfa793c8cf6383581e3c25d681
Craig Kelly <craig.n.kelly@gmail.com>
2015-03-09 14:37:18 -0500

    update reporting script for shuf/swap

--- scripts/test_export_music2.sh
+++ scripts/test_export_music2.sh
@@ -3,5 +3,5 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPT=$THIS_DIR/../mofacts/server/experiment_times.js
 
-#mongo --quiet local --eval "experiment='WorldTDF.xml'" $SCRIPT
-mongo --quiet local --eval "experiment='Music2.xml'" $SCRIPT
+mongo --quiet local --eval "experiment='SDTrainTest.xml'" $SCRIPT
+#mongo --quiet local --eval "experiment='Music2.xml'" $SCRIPT
