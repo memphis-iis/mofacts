6dfe92f6737d22a18ae4c74573581d2343eeba4e
Craig Kelly <craig.n.kelly@gmail.com>
2015-03-10 00:06:28 -0500

    image resizing for diplay sizes

--- mofacts/client/index.js
+++ mofacts/client/index.js
@@ -1,4 +1,36 @@
+function redoCardImage() {
+    //Note that just in case we can't get the height on the window we punt
+    //with a default that is reasonable a lot of the time
+    var wid = $(window).width() || 640;
+    var hgt = $(window).height() || 480;
+
+    if (wid > hgt) {
+        //Landscape - assume that we want the image to fit entirely along
+        //with the answer box on a fairly sane screen
+        hgt = Helpers.display(Math.floor(hgt * 0.45)) + "px";
+        wid = "auto";
+    }
+    else {
+        //Portrait - set the image to be the width of the screen. They'll
+        //probably need to scroll for tall images
+        hgt = "auto";
+        wid = "90%";
+    }
+
+    $("#cardTemplateQuestionImg")
+        .css("height", hgt)
+        .css("width", wid);
+}
+
 Meteor.startup(function() {
     Session.set("debugging", true);
     sessionCleanUp();
+
+    //Include any special jQuery handling we need
+    $(window).resize(function(evt) {
+        redoCardImage();
+    });
+    $("#cardTemplateQuestionImg").load(function(evt) {
+        redoCardImage();
+    });
 });
