8daedfada7e64bb809a64adfc399d55fc867305c
ppavlik <ppavlik@Saturn>
2015-05-28 13:27:55 -0500

    Local batch script

--- MoFaCTS ServerExport.bat
+++ MoFaCTS ServerExport.bat
@@ -1,8 +1,5 @@
 set PATH=%PATH%;C:\Program Files (x86)\Git\bin
 cd C:\Users\ppavlik\Documents\NetBeansProjects\mofacts
 vagrant up
-vagrant ssh
-cd mofacts/mofacts
-meteor bundle mofacts.tar.gz
-scp mofacts.tar.gz ppavlik@optimallearning.org:/var/www/mofacts
+vagrant ssh -c "cd mofacts/mofacts; meteor bundle mofacts.tar.gz; scp mofacts.tar.gz ppavlik@optimallearning.org:/var/www/mofacts"
 pause
