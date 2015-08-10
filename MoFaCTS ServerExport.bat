4ba80008ad5129ac53c57e7faa633a0e7f5b6b4e
Phil Pavlik <imrryr@gmail.com>
2015-08-10 12:35:08 -0500

    Bat file change

--- MoFaCTS ServerExport.bat
+++ MoFaCTS ServerExport.bat
@@ -1,8 +1,8 @@
 set PATH=%PATH%;C:\Program Files (x86)\Git\bin
 cd C:\Users\ppavlik\Documents\NetBeansProjects\mofacts\mofacts
 vagrant up
-vagrant ssh -c "cd mofacts/mofacts; meteor bundle mofacts.tar.gz"
-scp mofacts.tar.gz ppavlik@optimallearning.org:/var/www/mofacts"
+vagrant ssh -c "cd mofacts/mofacts; meteor build ./build --architecture os.linux.x86_64"
+scp build/mofacts.tar.gz ppavlik@optimallearning.org:/var/www/mofacts
 pause
 
 
