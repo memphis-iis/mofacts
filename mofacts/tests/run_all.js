d50b6c0af4db63186c4fb3f3c382f6557c385275
Craig Kelly <craig.n.kelly@gmail.com>
2015-10-30 14:28:09 -0500

    change all tests order

--- mofacts/tests/run_all.js
+++ mofacts/tests/run_all.js
@@ -6,8 +6,8 @@ suspend_reporting = true;  //No reporting until all suites are done
 
 //Test suite collections we want to run
 require('./AssessmentSessionTesting.js');
-require('./global_help_testing.js');
 require('./shuffle_swap_testing.js');
+require('./global_help_testing.js');
 
 suspend_reporting = false;
 test_report();
