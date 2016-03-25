23e653f3a9c0722fd45d7ef595cadc9686dd7e83
Nick G <nmg@homeserv>
2016-03-25 01:47:03 -0500

    The buttons in allStudents now use real data to determine the coloring.
    Former-commit-id: 50015978fcc2fa211084242a5b6d08d5626fbc6d

--- mofacts/client/views/allStudents.js
+++ mofacts/client/views/allStudents.js
@@ -56,7 +56,8 @@ Template.allStudents.rendered = function () {
 
 		// We have to build the query as a JavaScript object to get only the users that have done questions on this test.
 		var userQuery = {};
-		userQuery[getCurrentTdfName().replace(".", "_")] = {$exists: true};
+		var tdfDBName = getCurrentTdfName().replace(".", "_");
+		userQuery[tdfDBName] = {$exists: true};
     var currTdfUsers = UserMetrics.find(userQuery);
 		//
     var addButton = function(btnObj) {
@@ -68,17 +69,28 @@ Template.allStudents.rendered = function () {
     };
 
     currTdfUsers.forEach( function (user) {
-				// Cross-associate the username from Meteor.users to our list here.
-				user.username = Meteor.users.findOne({_id: user._id}, {username: 1}).username;
-				// Currently we are randomly assigning scores to the users. This will change in production.
-				user.score = randomScore();
-				//
+				// Fetch the username from the main database and associate it with our smaller listing here.
+				user.username = Meteor.users.findOne({_id: user._id}, {username: true}).username;
+
+				var computeUserScore = function(user) {
+						var indivUserQuery = {};
+						indivUserQuery['_id'] = user._id;
+						// We use findOne because there should only ever be one user with any given id.
+						var indivUser = UserMetrics.findOne(indivUserQuery);
+						var askCount = 0;
+						var correctCount = 0;
+						_.chain(indivUser).prop(tdfDBName).each( function (item) {
+								askCount = askCount + _.chain(item).prop('questionCount').intval().value();
+								correctCount = correctCount + _.chain(item).prop('correctAnswerCount').intval().value();
+						});
+						return correctCount/askCount;
+				};
+				
+				// Compute the user's average score across this system to appear on the button (and to color it).
+				user.score = computeUserScore(user);
 				
 				// For convenience only, we assign the index to a variable so the code down below is less messy.
 				var colorIndex = determineColorIndex(user.score);
-        function random(min, max) {
-            return Math.floor(Math.random() * (max-min)) + min; //Used to randomly generate numbers for color selection. Will be removed later
-        }
 
         addButton(
             $("<button type='button' id='"+user._id+"' name='"+user._id+"'></button>")
