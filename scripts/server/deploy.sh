3071c30da36216680671ff0628cff4d7b7139159
Craig Kelly <craig.n.kelly@gmail.com>
2016-04-11 13:58:45 -0500

    server script update

--- scripts/server/deploy.sh
+++ scripts/server/deploy.sh
@@ -3,14 +3,14 @@
 ################################################################
 # Configuration
 
-#variables used in this script
+# variables used in this script
 BUNDLE_NAME=mofacts.tar.gz
 BUNDLE_POSTDEP=$BUNDLE_NAME.deployed
 EXEC_USER=www-data
 
-#parameters for meteor
+# parameters for meteor
 export MONGO_URL='mongodb://localhost:27017/MoFaCT'
-#export ROOT_URL='http://mofacts.optimallearning.org'
+# export ROOT_URL='http://mofacts.optimallearning.org'
 export ROOT_URL='http://ec2-52-34-171-189.us-west-2.compute.amazonaws.com'
 export BIND_IP='127.0.0.1'
 export PORT='3000'
@@ -18,7 +18,7 @@ export PORT='3000'
 ################################################################
 # Initial checks
 
-#Must be root or able to sudo as root
+# Must be root or able to sudo as root
 if [ "$(id -u)" != "0" ]; then
     echo "Not running as root - will test..."
     if [ "$(sudo id -u)" != "0" ]; then
@@ -30,13 +30,13 @@ fi
 ################################################################
 # Implementation as functions
 
-#FUNCTION: Force shutdown of all nodejs processes
+# FUNCTION: Force shutdown of all nodejs processes
 function force_shutdown() {
     echo "Killing node processes"
     sudo killall node
 }
 
-#FUNCTION: Deployment functionality
+# FUNCTION: Deployment functionality
 function deploy() {
     echo "Deleting previous bundle"
     sudo rm -fr bundle
@@ -48,24 +48,17 @@ function deploy() {
     echo "Renaming to " $BUNDLE_POSTDEP
     sudo mv $BUNDLE_NAME $BUNDLE_POSTDEP
 
-    echo "Rebuilding bcrypt and fibers in bundle/programs/server"
+    echo "Rebuilding bundle/programs/server"
     pushd bundle/programs/server
     sudo npm install
-    sudo rm -fr node_modules/fibers
-    sudo rm -fr node_modules/bcrypt
-    sudo npm install fibers@1.0.1 bcrypt@0.7.7
-    popd
-    echo "Rebuilding npm-bcrypt in bundle/programs/server/npm/npm-bcrypt"
-    pushd bundle/programs/server/npm/npm-bcrypt
-    sudo rm -fr node_modules
-    sudo npm install bcrypt@0.7.7
+    sudo npm install bcrypt
     popd
 
     echo "Insuring ownership of all files"
     sudo chown -R $EXEC_USER:$EXEC_USER *
 }
 
-#FUNCTION: Execution functionality
+# FUNCTION: Execution functionality
 function execute() {
     #Run as www-data
     echo "NOTE: will execute as user " $EXEC_USER
