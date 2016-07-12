870ecfe886bb00aa0b2131c70f9a690a7fa4fb33
Craig Kelly <craig.n.kelly@gmail.com>
2016-07-12 14:13:09 -0500

    old script todo comment fix
    Former-commit-id: acffb461246c0d980fb3e3253b784e12a6c52b0e

--- scripts/prof_check.js
+++ scripts/prof_check.js
@@ -4,7 +4,7 @@ function pu(usr) {
     print("  " + JSON.stringify(usr, null, 2));
 }
 
-// TODO: this will need to change
+// IMPORTANT: this doesn't really work now that we're using google oauth
 db.users.find({'username':'ppavlik'}).forEach(function(usr){
     print('Found ' + usr._id + ' ' + usr.username);
     pu(usr);
