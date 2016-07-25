be65f2ecdd0ea9941ce054e4c7257337b65b4fd5
Craig Kelly <craig.n.kelly@gmail.com>
2016-07-25 15:47:58 -0500

    Insure "run all" test actually runs all tests
    Former-commit-id: f6b0fb107e5da611ba663df7ceff5d10f5686933

--- mofacts/tests/run_all.js
+++ mofacts/tests/run_all.js
@@ -5,9 +5,10 @@ require('./dinky_test.js');
 suspend_reporting = true;  //No reporting until all suites are done
 
 //Test suite collections we want to run
+require('./answer_assess_testing.js');
 require('./AssessmentSessionTesting.js');
-require('./shuffle_swap_testing.js');
 require('./global_help_testing.js');
+require('./shuffle_swap_testing.js');
 
 suspend_reporting = false;
 test_report();
