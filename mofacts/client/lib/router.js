0fd722bbbdf6556209a6f5f471ef62b07cce555e
Craig Kelly <craig.n.kelly@gmail.com>
2016-08-02 17:28:24 -0500

    Use cookies to handle experimental subjects hitting the refresh button
    Former-commit-id: 24474538d329f0e2b6052ea36f97c54e9d2cef59

--- mofacts/client/lib/router.js
+++ mofacts/client/lib/router.js
@@ -1,3 +1,61 @@
+/* router.js - the routing logic we use for the application.
+
+If you need to create a new route, note that you should specify a name and an
+action (at a minimum). This is good practice, but it also works around a bug
+in Chrome with certain versions of Iron Router (they routing engine we use).
+
+IMPORTANT: If you are routing someone to the signin screen (i.e. they need to
+log in) then you should call the routeToSignin function of calling Router.go
+directly. This is important to make sure that both "normal" logins *and*
+experimental participant (Mechanical Turk) logins function correctly.
+
+The routes are self-explanatory, but "loginMode" requires a little explanation.
+When a user enters the application via a URL of the form /experiment/{target}/{x}
+they are placed in "experiment" mode. The following changes are made:
+
+    * The session var "loginMode" is set to "experiment" (instead of "normal")
+    * The session var "experimentTarget" is set to whatever is specified in
+      {target} in the URL. This is required.
+    * The session var "experimentXCond" is set to whatever is specified in {x}
+      in the URL. This defaults to 0. The default value is used if the value
+      given cannot be interpreted as an int.
+    * The user is NOT shown the OAuth "Sign In With Google" screen. Instead a
+      screen for user ID entry (which should be their Turk ID) is shown instead
+    * The user isn't allowed to select a TDF - the TDF matching {target} is
+      always chosen. (This is specified in the <experimentTarget> tag in the
+      top TDF setspec section).
+
+As an example, if a user navigates to
+    https://mofacts.optimallearning.org/experiment/learning/1
+then the following things will happen:
+
+    * Session["loginMode"] == "experiment"
+    * Session["experimentTarget"] == "learning"
+    * Session["experimentXCond"] == "1"
+    * The user is asked for an ID
+    * After entering the ID, the user is taken to the TDF with
+      <experimentTarget>learning</experimentTarget> in it's setspec
+
+As an example of XCond defaults, the following URL's are ALL equivalent:
+
+    * https://mofacts.optimallearning.org/experiment/learning/
+    * https://mofacts.optimallearning.org/experiment/learning/0
+    * https://mofacts.optimallearning.org/experiment/learning/abc
+
+A major issue is that once a user enters in experiment mode, the routes (like
+instructions and card) look the same. If the user uses the browser's refresh
+function, our logic will take them to the "normal" sign in screen. Since we
+allow login via Gmail account this could be confusing. As a result, we now use
+a cookie scheme to insure experimental participants stay in experiment mode:
+
+    * When a user first hits the URL /experiment/{target}/{x} we write cookies
+      (with expiration set so that they outlast the current browser session)
+    * Whenever routeToSignin is called, we check the cookies. If they are set
+      then we reconstruct the experiment session variables as above.
+    * If a user visits the root ("/") route, we reset all cookies back in order
+      to allow "normal" login again.
+*/
+
 // Note that these three session variables aren't touched by the helpers in
 // lib/sessionUtils.js. They are only set here in our client-side routing
 Session.set("loginMode", "normal");
@@ -5,7 +63,17 @@ Session.set("experimentTarget", "");
 Session.set("experimentXCond", "");
 Session.set("clusterMapping", "");
 
+
 routeToSignin = function() {
+    // If the isExperiment cookie is set we always for experiment mode. This
+    // handles an experimental participant refreshing the browser
+    var expCookie = _.chain(Cookie.get("isExperiment")).trim().intval().value();
+    if (expCookie) {
+        Session.set("loginMode", "experiment");
+        Session.set("experimentTarget", Cookie.get("experimentTarget"));
+        Session.set("experimentXCond", Cookie.get("experimentXCond"));
+    }
+
     if (Session.get("loginMode") === "experiment") {
         var routeParts = ['/experiment'];
 
@@ -32,13 +100,18 @@ Router.configure({
 Router.route('/experiment/:target?/:xcond?', {
     name: "client.experiment",
     action: function() {
-        Session.set("loginMode", "experiment");
-
+        // We set our session variable and also set a cookie (so that we still
+        // know they're an experimental participant after browser refresh)
         var target = this.params.target || "";
         var xcond = this.params.xcond || "";
 
+        Session.set("loginMode", "experiment");
         Session.set("experimentTarget", target);
         Session.set("experimentXCond", xcond);
+
+        Cookie.set("isExperiment", "1", 21);  // 21 days
+        Cookie.set("experimentTarget", target, 21);
+        Cookie.set("experimentXCond", xcond, 21);
 
         console.log("EXPERIMENT target:", target, "xcond", xcond);
 
@@ -65,7 +138,11 @@ Router.route('/signup', {
 Router.route('/', {
     name: "client.index",
     action: function () {
-        //this.render('signIn');
+        // If they are navigating to "/" then we clear the (possible) cookie
+        // keeping them in experiment mode
+        Cookie.set("isExperiment", "0", 1);  // 1 day
+        Cookie.set("experimentTarget", "", 1);
+        Cookie.set("experimentXCond", "", 1);
         this.render('signInOauth');
     }
 });
