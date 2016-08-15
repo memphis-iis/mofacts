5c2a7a98e7abbbc2ffb72dd3cd95ecbd4ecfbb0a
Craig Kelly <craig.n.kelly@gmail.com>
2016-08-15 10:54:53 -0500

    Adding hack script to fix deployment issues
    Former-commit-id: 1b0deaca6ae1a6b350057863a201251140eb6c11

---
+++ mofacts/hack_scss.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Insuring git installed"
+sudo apt-get install -y git
+
+echo "Insuring proper packages folder"
+sudo umount .meteor/local -f
+rm .meteor/local -rf
+mkdir -p .meteor/local
+
+sudo umount packages -f
+rm packages -rf
+mkdir -p packages
+
+mkdir -p $HOME/.meteor/local
+sudo mount --bind $HOME/.meteor/local .meteor/local
+
+mkdir -p $HOME/.meteor/packages
+sudo mount --bind $HOME/.meteor/packages packages
+
+if [ ! -d packages ]; then
+    echo "packages directory does not exist - exiting"
+    exit 1
+fi
+cd packages
+
+echo "Manually getting scss"
+git clone https://github.com/fourseven/meteor-scss.git
+
+echo "If everything worked, you should be able to run:"
+echo "./run_meteor update"
+#!/bin/bash
+
