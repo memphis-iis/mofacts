e6795f112786007ea35dccb0cf83aaeeffc28501
Andrew Tackett <andrew.tackett@gmail.com>
2019-07-11 12:48:28 -0500

    make enter button press on dev environment hit test signin button
    Former-commit-id: b8880acd2314493234d15d11bafce416ac11c8ec

--- mofacts/client/views/signInOauth.js
+++ mofacts/client/views/signInOauth.js
@@ -2,6 +2,72 @@ function testUserEnabled() {
     return _.chain(Meteor.settings).prop("public").prop("testLogin").value();
 }
 
+function testLogin(){
+  console.log("TEST Login");
+
+  // Just a sanity check
+  if (!testUserEnabled()) {
+      console.log("TEST Login REJECTED");
+      $("#testSignInButton").prop('disabled', false);
+      return;
+  }
+
+  var testUserName = _.trim($("#testUsername").val()).toUpperCase();
+  if (!testUserName) {
+      console.log("No TEST user name specified");
+      alert("No TEST user name specified");
+      $("#testSignInButton").prop('disabled', false);
+      return;
+  }
+
+  var testPassword = Helpers.blankPassword(testUserName);
+
+  Meteor.call("signUpUser", testUserName, testPassword, true, function(error, result) {
+      var errorMsgs = [];
+
+      if (typeof error !== "undefined") {
+          errorMsgs.push(error);
+      }
+
+      if (!!result && result.length) {
+          _.each(result, function(msg) {
+              errorMsgs.push(msg);
+          });
+      }
+
+      //If there was a call failure or server returned error message,
+      //then we can't proceed
+      if (errorMsgs.length > 0) {
+          var errorText = displayify(errorMsgs);
+          console.log("Experiment user login errors:", errorText);
+          alert("Experiment user login errors:", errorText);
+          $("#testSignInButton").prop('disabled', false);
+          return;
+      }
+
+      sessionCleanUp();
+
+      // Note that we force Meteor to think we have a user name so that
+      // it doesn't try it as an email - this let's you test email-like
+      // users, which you can promote to admin or teacher
+      Meteor.loginWithPassword({'username': testUserName}, testPassword, function(error) {
+          if (typeof error !== 'undefined') {
+              console.log("ERROR: The user was not logged in on TEST sign in?", testUserName, "Error:", error);
+              alert("It appears that you couldn't be logged in as " + testUserName);
+              $("#testSignInButton").prop('disabled', false);
+          }
+          else {
+              if (Session.get("debugging")) {
+                  var currentUser = Meteor.users.findOne({_id: Meteor.userId()}).username;
+                  console.log(currentUser + " was test logged in successfully! Current route is ", Router.current().route.getName());
+                  Meteor.call("debugLog", "TEST Sign in was successful - YOU SHOULD NOT SEE THIS IN PRODUCTION");
+              }
+              Router.go("/profile");
+          }
+      });
+  });
+}
+
 var waitOnConfig = function(){
   if(!Accounts.loginServicesConfigured()){
     console.log('+++++ Config not loaded! Waiting... +++++');
@@ -51,71 +117,18 @@ Template.signInOauth.events({
         });
     },
 
+    'keypress .accept-enter-key' : function (event) {
+        var key = event.keyCode || event.which;
+        if (key == 13) {
+            event.preventDefault();
+            $("#testSignInButton").prop("disabled",true);
+            testLogin();
+        }
+    },
+
     'click #testSignInButton': function(event) {
         $("#testSignInButton").prop('disabled', true);
         event.preventDefault();
-        console.log("TEST Login");
-
-        // Just a sanity check
-        if (!testUserEnabled()) {
-            console.log("TEST Login REJECTED");
-            $("#testSignInButton").prop('disabled', false);
-            return;
-        }
-
-        var testUserName = _.trim($("#testUsername").val()).toUpperCase();
-        if (!testUserName) {
-            console.log("No TEST user name specified");
-            alert("No TEST user name specified");
-            $("#testSignInButton").prop('disabled', false);
-            return;
-        }
-
-        var testPassword = Helpers.blankPassword(testUserName);
-
-        Meteor.call("signUpUser", testUserName, testPassword, true, function(error, result) {
-            var errorMsgs = [];
-
-            if (typeof error !== "undefined") {
-                errorMsgs.push(error);
-            }
-
-            if (!!result && result.length) {
-                _.each(result, function(msg) {
-                    errorMsgs.push(msg);
-                });
-            }
-
-            //If there was a call failure or server returned error message,
-            //then we can't proceed
-            if (errorMsgs.length > 0) {
-                var errorText = displayify(errorMsgs);
-                console.log("Experiment user login errors:", errorText);
-                alert("Experiment user login errors:", errorText);
-                $("#testSignInButton").prop('disabled', false);
-                return;
-            }
-
-            sessionCleanUp();
-
-            // Note that we force Meteor to think we have a user name so that
-            // it doesn't try it as an email - this let's you test email-like
-            // users, which you can promote to admin or teacher
-            Meteor.loginWithPassword({'username': testUserName}, testPassword, function(error) {
-                if (typeof error !== 'undefined') {
-                    console.log("ERROR: The user was not logged in on TEST sign in?", testUserName, "Error:", error);
-                    alert("It appears that you couldn't be logged in as " + testUserName);
-                    $("#testSignInButton").prop('disabled', false);
-                }
-                else {
-                    if (Session.get("debugging")) {
-                        var currentUser = Meteor.users.findOne({_id: Meteor.userId()}).username;
-                        console.log(currentUser + " was test logged in successfully! Current route is ", Router.current().route.getName());
-                        Meteor.call("debugLog", "TEST Sign in was successful - YOU SHOULD NOT SEE THIS IN PRODUCTION");
-                    }
-                    Router.go("/profile");
-                }
-            });
-        });
+        testLogin();
     }
 });
