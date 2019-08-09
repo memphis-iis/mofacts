6877e4202d11e6b3b08f60e5fd300131b9768e87
Andrew Collin Tackett (ctckett1) <ctckett1@memphis.edu>
2019-08-09 10:26:07 -0500

    fix mongodb not installing due to gpg keys no longer being available
    Former-commit-id: 77f7a63ca976e460cc7526b6b61676dc4456fa22

--- .provisioning_script.sh
+++ .provisioning_script.sh
@@ -20,7 +20,8 @@ sudo apt-get install -y build-essential gcc g++ make automake git
 
 # Use MongoDB 3.2
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D68FA50FEA312927
+echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
