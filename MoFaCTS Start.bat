a1911bb13981a284073441c240be602854fea713
Phil Pavlik <imrryr@gmail.com>
2019-08-19 07:46:34 -0500

    Test tdf and some schedule algorithm changes

--- MoFaCTS Start.bat
+++ MoFaCTS Start.bat
@@ -1,7 +1,6 @@
-set PATH=%PATH%;C:\Users\ppavl\AppData\Local\Programs\Git\usr\bin
-cd C:\Users\ppavl\Dropbox\Active projects\mofacts
+
+cd C:\Users\ppavl\Dropbox\Active projects\mofacts_IIS
 vagrant up
 vagrant ssh
 cd mofacts/mofacts
 ./run_meteor
-   
