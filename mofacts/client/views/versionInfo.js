6402d2fc7263a58cd35f4d880884836aea3c5489
Andrew Tackett <andrew.tackett@gmail.com>
2019-11-14 15:50:11 -0600

    change error reports to be emailed at 9am CST

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"4f0458e2383856e0b3af01401ec9fcadfed0b852",
-"buildTimestamp":"Thu Nov 14 11:06:14 CST 2019",
+"commitID":"ea19b86d01be35d45a9a3c00e2f42f7bd078d2e6",
+"buildTimestamp":"Thu Nov 14 15:45:50 CST 2019",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
