39b8a574ccd4a8b5e9f739fc96c4f90ccc06e18d
Andrew Tackett <andrew.tackett@gmail.com>
2019-11-14 11:05:31 -0600

    fix new answer code so that it works with answers with bar (|) in them that distinguish multiple correct spellings of an answer

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"0b3a17fe698eeac60b9963441d9b58efe74db7fb",
-"buildTimestamp":"Wed Nov 13 14:48:37 CST 2019",
+"commitID":"9dff3d6b0478c4b8e0d85a5875ce1b2c53b9bf02",
+"buildTimestamp":"Thu Nov 14 10:18:57 CST 2019",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
