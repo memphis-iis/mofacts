82639d4d8b3c8fbe5260a44222bbc0a38ed650f0
Andrew Tackett <andrew.tackett@gmail.com>
2019-11-14 15:45:22 -0600

    correct answer assessment not working ideally for stims with branches & multiple spellings of correct answers

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"9dff3d6b0478c4b8e0d85a5875ce1b2c53b9bf02",
-"buildTimestamp":"Thu Nov 14 10:18:57 CST 2019",
+"commitID":"4f0458e2383856e0b3af01401ec9fcadfed0b852",
+"buildTimestamp":"Thu Nov 14 11:06:14 CST 2019",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
