914b32c3141c528f694e4ed2c4dd47657f54d719
wscarter <wcarter.dev@gmail.com>
2020-01-03 13:56:09 -0600

    remove unnecessary package remove

--- .provisioning_script.sh
+++ .provisioning_script.sh
@@ -28,7 +28,6 @@ sudo apt-get install -y git
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
-sudo apt-get remove -y mongodb-org && sudo apt-get autoremove -y
 sudo apt-get install -y mongodb-org
 
 sudo apt-get install dos2unix
