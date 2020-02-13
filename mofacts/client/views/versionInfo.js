3502212938be6ec0fba6d5d713bb0ee16cf8d493
Andrew Tackett <andrew.tackett@gmail.com>
2020-02-13 15:13:46 -0600

    some progess on SW SSO

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"6402d2fc7263a58cd35f4d880884836aea3c5489",
-"buildTimestamp":"Thu Nov 14 15:50:25 CST 2019",
+"commitID":"985ce488d9437a2353078a618531e41fd1fb7a87",
+"buildTimestamp":"Wed Feb 12 15:35:15 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
