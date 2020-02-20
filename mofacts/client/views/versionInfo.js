1bb3e3b31da3431973406de72dafc21dd2ec72c8
Andrew Tackett <andrew.tackett@gmail.com>
2020-02-20 15:49:19 -0600

    switch login from mofacts to southwest sso on southwest login page

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"8afc56de291e1897083cba3751822d1058ab9e3a",
-"buildTimestamp":"Thu Feb 13 17:55:10 CST 2020",
+"commitID":"eef6a7fab31abab9d36e2c5f64ab37213891fe1a",
+"buildTimestamp":"Tue Feb 18 16:05:51 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
