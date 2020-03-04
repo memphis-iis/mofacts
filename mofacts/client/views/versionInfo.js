d57ea19f8b823309b28efa1c5f55b1ba82d8a0d3
Andrew Tackett <andrew.tackett@gmail.com>
2020-03-03 22:13:48 -0600

    fix but in replaceClozeWithSyllables when question is undefined

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"1bb3e3b31da3431973406de72dafc21dd2ec72c8",
-"buildTimestamp":"Fri Feb 21 16:11:43 CST 2020",
+"commitID":"88d2ee7fe83077da7f909b0bdd3bf8fae1da7fca",
+"buildTimestamp":"Tue Mar  3 21:52:36 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
