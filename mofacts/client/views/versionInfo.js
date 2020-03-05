5cffd2a1c627e8e64897d9bf51eb5ad21f12dc94
Andrew Tackett <andrew.tackett@gmail.com>
2020-03-04 18:24:36 -0600

    partial progress on student reporting

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"43bfd5499e5b1c2cb88ba5c08e19518a620ea011",
-"buildTimestamp":"Wed Mar  4 07:17:20 CST 2020",
+"commitID":"a93f743f0e3f7e0cd39cbbc4f885b24e3d08b7b0",
+"buildTimestamp":"Wed Mar  4 16:15:17 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
