0d4757df85bc438e343a9a6dfa053e627750f95c
Andrew Tackett <andrew.tackett@gmail.com>
2020-03-05 15:13:00 -0600

    update student reporting with same fix as card for subword answers not restoring properly if you navigate away and back before answering

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"5413684ab254bb86fffd32ea710c08f2942b9d7f",
-"buildTimestamp":"Thu Mar  5 11:07:01 CST 2020",
+"commitID":"ba561dde064ff1fba986151a29a1e30e83db0350",
+"buildTimestamp":"Thu Mar  5 15:10:40 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
