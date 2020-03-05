4f00c6ccc21a2a131a3445c726b7b663832106ca
Andrew Tackett <andrew.tackett@gmail.com>
2020-03-05 11:06:48 -0600

    fix cards not being set to be usable because !!0 == false in javascript

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"a93f743f0e3f7e0cd39cbbc4f885b24e3d08b7b0",
-"buildTimestamp":"Wed Mar  4 16:15:17 CST 2020",
+"commitID":"75e32fb80335d8f38e4b3a313fb30b0a58281831",
+"buildTimestamp":"Wed Mar  4 21:11:19 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
