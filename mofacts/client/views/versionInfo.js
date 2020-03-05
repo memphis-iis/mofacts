4ef9f4e13efd7e2f48c6cd17273478bd7cd0d374
Andrew Tackett <andrew.tackett@gmail.com>
2020-03-05 14:41:14 -0600

    change student progress reporting to only show exact stims we practiced, not all stims in the cluster

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"75e32fb80335d8f38e4b3a313fb30b0a58281831",
-"buildTimestamp":"Wed Mar  4 21:11:19 CST 2020",
+"commitID":"5413684ab254bb86fffd32ea710c08f2942b9d7f",
+"buildTimestamp":"Thu Mar  5 11:07:01 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
