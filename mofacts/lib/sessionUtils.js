e057e56629e13d7c12ca5acdc62314764b385a1a
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-03 20:34:48 -0500

    add kaitlyn feature allowing alternating image stimuli depending on even/odd condition group

--- mofacts/lib/sessionUtils.js
+++ mofacts/lib/sessionUtils.js
@@ -81,6 +81,7 @@ sessionCleanUp = function() {
     Session.set("experimentPasswordRequired",false);
     Session.set("filter","@gmail.com");
     Session.set("ignoreOutOfGrammarResponses",false);
+    Session.set("imageFilteringConditionGroup",false);
     Session.set("lastTimestamp", 0);
     Session.set("needResume", false);
     Session.set("questionIndex", undefined);
