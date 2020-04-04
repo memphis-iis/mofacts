be8b89defb28b33efd18bb2d65537481a43b1187
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-03 20:45:38 -0500

    remove tdf/stim file that was accidentally uploaded

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"d8a53508e0fea09c9a2fcb7bba48e6dd85863bad",
-"buildTimestamp":"Wed Apr  1 11:23:29 CDT 2020",
+"commitID":"6e6c9b1f682d4337183664f5b668082f53315233",
+"buildTimestamp":"Fri Apr  3 20:35:48 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
