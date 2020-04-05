57f02d70294c2cf216d0e95fca350b5cdd008304
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-05 18:52:28 -0500

    add images for kaitlyns chapter 9 experiment

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"6e6c9b1f682d4337183664f5b668082f53315233",
-"buildTimestamp":"Fri Apr  3 20:35:48 CDT 2020",
+"commitID":"41eb9555e6f56fb6408d1500558961dad43553e0",
+"buildTimestamp":"Fri Apr  3 20:45:52 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
