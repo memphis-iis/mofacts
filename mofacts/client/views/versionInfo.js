aca783529020fdab0219867d1360b1d2b98d8dcf
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-06 16:49:42 -0500

    resize images for chapter 9

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"57f02d70294c2cf216d0e95fca350b5cdd008304",
-"buildTimestamp":"Sun Apr  5 18:54:24 CDT 2020",
+"commitID":"3d6f67c4b1cfbd367bfad01c9f795e511533f3d0",
+"buildTimestamp":"Mon Apr  6 16:20:53 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
