ff243d81f90637f8f37138a1be1d302809fdc62d
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-06 16:20:40 -0500

    fix html tag rendering issue when partial syllable hints happen

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"41eb9555e6f56fb6408d1500558961dad43553e0",
-"buildTimestamp":"Fri Apr  3 20:45:52 CDT 2020",
+"commitID":"57f02d70294c2cf216d0e95fca350b5cdd008304",
+"buildTimestamp":"Sun Apr  5 18:54:24 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
