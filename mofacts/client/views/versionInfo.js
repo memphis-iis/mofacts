f6b968fb1dd6b9d0a6c98b0859eb66c7d1b6912d
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-10 12:19:49 -0500

    add images for chapter 10

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"3d6f67c4b1cfbd367bfad01c9f795e511533f3d0",
-"buildTimestamp":"Mon Apr  6 16:20:53 CDT 2020",
+"commitID":"79c921dc69a6c976d85b634576cc212beb7faf85",
+"buildTimestamp":"Mon Apr  6 16:50:14 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
