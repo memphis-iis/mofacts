65a8ce873913c31dbe7e0c8264f18e1a93cbfa51
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-13 11:55:47 -0500

    make checking against other answers for incorrect user answer a delivery param configurable option

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"79c921dc69a6c976d85b634576cc212beb7faf85",
-"buildTimestamp":"Mon Apr  6 16:50:14 CDT 2020",
+"commitID":"f970000a93c526a9d1d850a48038086e9a22968b",
+"buildTimestamp":"Sun Apr 12 19:20:21 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
