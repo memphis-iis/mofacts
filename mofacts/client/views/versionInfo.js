19420c5316c9ffd726d0625a2a09ed784920e2e0
Andrew Tackett <andrew.tackett@gmail.com>
2020-04-22 15:43:29 -0500

    make sure tdfs without hintsylls can function properly

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"f970000a93c526a9d1d850a48038086e9a22968b",
-"buildTimestamp":"Sun Apr 12 19:20:21 CDT 2020",
+"commitID":"1fad54dd2331ac7a155bd79192f3833e8cf46672",
+"buildTimestamp":"Mon Apr 13 11:56:02 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
