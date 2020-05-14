09cc92b8b6d8191d858c5383f387ff546e7bdba0
Andrew Tackett <andrew.tackett@gmail.com>
2020-05-14 14:52:22 -0500

    changing out assets and minor repo cleanup

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"1fad54dd2331ac7a155bd79192f3833e8cf46672",
-"buildTimestamp":"Mon Apr 13 11:56:02 CDT 2020",
+"commitID":"80f713fedf5d9f918c70331cbde0faf4fb78f7aa",
+"buildTimestamp":"Mon Apr 27 15:29:27 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
