1d25aa419a132614c5c18812460dcd2f3e087595
Andrew Tackett <andrew.tackett@gmail.com>
2020-06-01 16:29:43 -0500

    fix syllable information on data download

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"65fd62288a6d0b6579f5e1aa19483b4831fc20fd",
-"buildTimestamp":"Thu May 21 13:28:18 CDT 2020",
+"commitID":"da473fd6ca6addf6bac15d9330b76951cb371128",
+"buildTimestamp":"Sat May 30 14:31:30 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
