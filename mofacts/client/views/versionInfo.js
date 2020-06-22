4df545315b202b0a990d94c1c482d710c8a40867
Andrew Tackett <andrew.tackett@gmail.com>
2020-06-21 22:22:26 -0500

    update transition statements to latest version

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"d3f44f5ed504e2bd97358b45053851758db2d9b1",
-"buildTimestamp":"Tue Jun  2 16:36:48 CDT 2020",
+"commitID":"33d0f6a68a77ba43e3d1de002036e332ea382fc7",
+"buildTimestamp":"Sun Jun 21 22:18:46 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
