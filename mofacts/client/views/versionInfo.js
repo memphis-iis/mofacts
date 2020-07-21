9c93a688715e2591284317f10d55e57e48948a2c
Andrew Tackett <andrew.tackett@gmail.com>
2020-07-21 16:50:20 -0500

    remove maintenance lock due to issue being fixed

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"435d3cf0cd79c0e7400d0aaed40c42c04bcc263b",
-"buildTimestamp":"Tue Jul 21 10:27:18 CDT 2020",
+"commitID":"18c8effe965fe9d90e019517062b337eb1d3d2ae",
+"buildTimestamp":"Tue Jul 21 16:45:07 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
