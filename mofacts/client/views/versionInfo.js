28eacd175c9d269c49fdf80d086b8db1dc36fb0a
Andrew Tackett <andrew.tackett@gmail.com>
2020-07-23 07:38:09 -0500

    clean up datashop report output column headers

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"9c93a688715e2591284317f10d55e57e48948a2c",
-"buildTimestamp":"Tue Jul 21 16:50:34 CDT 2020",
+"commitID":"81f687cd8eee39f5cc9c5dab6717ca580bb621dc",
+"buildTimestamp":"Thu Jul 23 06:45:39 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
