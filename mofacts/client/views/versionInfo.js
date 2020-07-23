e92bc5f61e9195fe0b4d8d8e15ba8a5e66efb2d8
Andrew Tackett <andrew.tackett@gmail.com>
2020-07-23 06:24:31 -0500

    skip review with dialogue feedback type

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"18c8effe965fe9d90e019517062b337eb1d3d2ae",
-"buildTimestamp":"Tue Jul 21 16:45:07 CDT 2020",
+"commitID":"9c93a688715e2591284317f10d55e57e48948a2c",
+"buildTimestamp":"Tue Jul 21 16:50:34 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
