bdaaa96031f7b57a7c42d0435a18864c3e75cd7f
Andrew Tackett <andrew.tackett@gmail.com>
2020-07-27 14:39:03 -0500

    fix cloze fill ins breaking due to mutation of cached stims

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"c5a6b6c83b90c12a289264594c3c9b5758c81502",
-"buildTimestamp":"Thu Jul 23 07:38:29 CDT 2020",
+"commitID":"b56cef016cee530aac088420312414373df11571",
+"buildTimestamp":"Sun Jul 26 11:23:18 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
