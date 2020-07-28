8d12944dc3732b4df0eb02788c2c6e2758b8dcfc
Andrew Tackett <andrew.tackett@gmail.com>
2020-07-28 13:13:35 -0500

    reopen to southwest students

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"b5555f917d542526ba88401aa7d92aa672027db6",
-"buildTimestamp":"Mon Jul 27 18:55:47 CDT 2020",
+"commitID":"9ddfe4663715dec844e0f7935da7aeabac657940",
+"buildTimestamp":"Tue Jul 28 12:16:38 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
