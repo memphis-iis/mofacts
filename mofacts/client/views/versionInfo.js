c171061cf2edc4ab7de77a17ae4a35df5de64bc4
Andrew Tackett <andrew.tackett@gmail.com>
2020-08-14 03:03:05 -0500

    allow multiple file uploads at once on content upload, also fix bugs related to new stim file format on uploading

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"0554950e37fddc2a087e9d80c1d13b66125bd161",
-"buildTimestamp":"Wed Aug 12 23:26:11 CDT 2020",
+"commitID":"1491505e2a1d2e5a39afc7eea2c101444853c616",
+"buildTimestamp":"Thu Aug 13 23:13:39 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
