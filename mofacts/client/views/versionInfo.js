604b97c469373d99dfa2367dab0a306b27f85f98
Andrew Tackett <andrew.tackett@gmail.com>
2020-08-24 11:55:34 -0500

    fix bug where refutational feedback return object isnt accessed correctly resulting in [object Object] on screen

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"8ae6e68bf5078dee5916f45edb3f0b8aa939da48",
-"buildTimestamp":"Sat Aug 22 05:28:03 CDT 2020",
+"commitID":"6271cd3972abd89231efb5ba5cae5012d1b8a36e",
+"buildTimestamp":"Sat Aug 22 12:05:21 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
