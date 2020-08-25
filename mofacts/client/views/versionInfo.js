9425591a5ecf315cfe6a9c93464d57ebe87b08c3
Andrew Tackett <andrew.tackett@gmail.com>
2020-08-24 21:42:14 -0500

    remove vertical space for improved ux on content gen

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"604b97c469373d99dfa2367dab0a306b27f85f98",
-"buildTimestamp":"Mon Aug 24 11:58:40 CDT 2020",
+"commitID":"4e03823a793d240ae862dd9620ca155d3031097c",
+"buildTimestamp":"Mon Aug 24 17:16:22 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
