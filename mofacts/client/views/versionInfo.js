cde223f27a5119c4f2ea32d19a2e42b25b448963
Andrew Tackett <andrew.tackett@gmail.com>
2020-08-25 11:21:27 -0500

    disable delete checkboxes for now as they arent working

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,10 +1,10 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"4e03823a793d240ae862dd9620ca155d3031097c",
-"buildTimestamp":"Mon Aug 24 17:16:22 CDT 2020",
+"commitID":"9425591a5ecf315cfe6a9c93464d57ebe87b08c3",
+"buildTimestamp":"Mon Aug 24 21:42:30 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
-"repoURL":"https://github.com/memphis-iis/mofacts-ies.git"
+"repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
 }
 );
