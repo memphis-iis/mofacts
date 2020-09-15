7417a67d95bdef07ec57dcc398a5f8066612a7ce
Andrew Tackett <andrew.tackett@gmail.com>
2020-09-15 18:56:17 -0500

    bring system back up

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"aa782342e1eb1798971bedafb4d46d802f6596b0",
-"buildTimestamp":"Wed Sep  9 15:36:53 CDT 2020",
+"commitID":"1e419662e1401f4d7e5e40d3aa2dc787a3b4630a",
+"buildTimestamp":"Tue Sep 15 17:21:20 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
