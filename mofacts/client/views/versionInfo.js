cf2e900314b59499e7dd90ea55ddf2e7cd1f325e
Andrew Tackett <andrew.tackett@gmail.com>
2020-09-16 13:07:55 -0500

    update load limit message

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"1e419662e1401f4d7e5e40d3aa2dc787a3b4630a",
-"buildTimestamp":"Tue Sep 15 17:21:20 CDT 2020",
+"commitID":"f1e5492588fa26cfb48d04e2892a274e748c971a",
+"buildTimestamp":"Tue Sep 15 18:56:32 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
