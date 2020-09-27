d5d8ca3665c41eca76595d76a2d1433a7eeb9bcb
Andrew Tackett <andrew.tackett@gmail.com>
2020-09-27 00:42:49 -0500

    sso cleanup and additional logging for troubleshooting

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"98ee84bffb59911ce42642cac01e925c4c13712e",
-"buildTimestamp":"Wed Sep 16 13:08:03 CDT 2020",
+"commitID":"36d6fa770b2c53fe5853c34a497cd04829c80d5a",
+"buildTimestamp":"Fri Sep 25 18:54:10 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
