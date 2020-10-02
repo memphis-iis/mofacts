6d851ab84f736447e24693e55d29a9e4f23c86fe
Andrew Tackett <andrew.tackett@gmail.com>
2020-10-02 15:28:23 -0500

    revert previous revert, as it did reintroduce a bug

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"36d6fa770b2c53fe5853c34a497cd04829c80d5a",
-"buildTimestamp":"Fri Sep 25 18:54:10 CDT 2020",
+"commitID":"dc1883f6607a1fd0c66b97e8d55f14b3a5a9150e",
+"buildTimestamp":"Thu Oct  1 22:35:42 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
