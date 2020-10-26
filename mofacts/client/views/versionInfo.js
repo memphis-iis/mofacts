44e54002baaf273e00acf5ab2d205d03a7826f0b
Andrew Tackett <andrew.tackett@gmail.com>
2020-10-26 14:31:29 -0500

    automatically fix line endings in run_meteor and ensure syllable service is running on boot up of dev vm

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"ec72c937524d176d8b8d58f9f66d5cb99a74b4b3",
-"buildTimestamp":"Mon Oct 12 16:54:51 CDT 2020",
+"commitID":"8b3c852283558ef69c94c9037fb4ee59a0675b5c",
+"buildTimestamp":"Sun Oct 25 21:03:57 CDT 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
