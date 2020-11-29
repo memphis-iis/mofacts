cb549385e0a7e15e2b0781a94b2e0c2398d2041c
Andrew Tackett <andrew.tackett@gmail.com>
2020-11-28 18:28:05 -0600

    change default message to checking system status instead of system is down

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"8b3c852283558ef69c94c9037fb4ee59a0675b5c",
-"buildTimestamp":"Sun Oct 25 21:03:57 CDT 2020",
+"commitID":"5baee3a252f76280241b8b5f0f954d946c1fad1f",
+"buildTimestamp":"Wed Nov 25 17:18:14 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
