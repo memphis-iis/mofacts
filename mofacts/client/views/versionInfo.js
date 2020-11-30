e2a02f857f92db21981ae8a6f8da3ed72fb8945e
Andrew Tackett <andrew.tackett@gmail.com>
2020-11-30 13:07:51 -0600

    make metadata url match RPT file so we can auto update certs

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"5baee3a252f76280241b8b5f0f954d946c1fad1f",
-"buildTimestamp":"Wed Nov 25 17:18:14 CST 2020",
+"commitID":"d4c58709630ee2951a545c76823378b3de7efb87",
+"buildTimestamp":"Mon Nov 30 12:45:53 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
