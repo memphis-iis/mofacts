e9670d0d8b088f124d46f310f86f2a7f1fda9365
Andrew Tackett <andrew.tackett@gmail.com>
2020-12-01 14:00:13 -0600

    move cache clear message to be more UX friendly

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"d4c58709630ee2951a545c76823378b3de7efb87",
-"buildTimestamp":"Mon Nov 30 12:45:53 CST 2020",
+"commitID":"e2a02f857f92db21981ae8a6f8da3ed72fb8945e",
+"buildTimestamp":"Mon Nov 30 15:52:14 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
