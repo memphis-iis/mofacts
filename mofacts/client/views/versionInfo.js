c69453f512c6a496e0449308012de830bf6a5b7a
Andrew Tackett <andrew.tackett@gmail.com>
2020-12-02 12:52:50 -0600

    add tutorial link for private browsing workaround

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"e2a02f857f92db21981ae8a6f8da3ed72fb8945e",
-"buildTimestamp":"Mon Nov 30 15:52:14 CST 2020",
+"commitID":"efcac517fc0e991202a38c20ad9d8ceba0c1f1a4",
+"buildTimestamp":"Wed Dec  2 12:34:05 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
