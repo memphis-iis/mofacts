8cac158564b15bb5ff5651b15d1b3ab4ef43303a
Andrew Tackett <andrew.tackett@gmail.com>
2020-12-03 04:58:16 -0600

    get metadata to exactly match working mofacts2 metadata

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"efcac517fc0e991202a38c20ad9d8ceba0c1f1a4",
-"buildTimestamp":"Wed Dec  2 12:34:05 CST 2020",
+"commitID":"6b2616b716b892eb98883eae048082ae22638460",
+"buildTimestamp":"Wed Dec  2 12:52:57 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
