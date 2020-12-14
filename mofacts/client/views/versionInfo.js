efd1c3a032b63573d7d4910d765f59efc452cc13
Andrew Tackett <andrew.tackett@gmail.com>
2020-12-14 02:24:51 -0600

    add deliveryparam numButtonListImageColumns to define the number of columns in button trials with images

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"6b2616b716b892eb98883eae048082ae22638460",
-"buildTimestamp":"Wed Dec  2 12:52:57 CST 2020",
+"commitID":"418580781cb50330e67999bd8aa12ba2a181a278",
+"buildTimestamp":"Mon Dec 14 00:47:44 CST 2020",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
