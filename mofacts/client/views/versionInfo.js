74b041ae2a2b2e3e214e345c8017ccec6c636671
Andrew Tackett <andrew.tackett@gmail.com>
2021-01-13 12:08:28 -0600

    fix bug with audio input always being enabled in experiment

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"77ed82ed00705a7576793a8293359790ed62e42c",
-"buildTimestamp":"Wed Dec 16 21:05:12 CST 2020",
+"commitID":"669e138961a0ebb25c914d731d469c751b425ef9",
+"buildTimestamp":"Wed Jan 13 11:40:01 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
