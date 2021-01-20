fc05c9f3be94d5c40250f6a8164188c65ae5f551
Andrew Tackett <andrew.tackett@gmail.com>
2021-01-19 20:50:51 -0600

    fix new tdfs not loading into content gen template select correctly, fix edited clozes not displaying correct original version

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"52d0c1fe3eceea52f7ca1147ad5634d3fa61b372",
-"buildTimestamp":"Mon Jan 18 21:35:55 CST 2021",
+"commitID":"ad57c8d7493175a888ea2bd4ce1a309fc50101bf",
+"buildTimestamp":"Mon Jan 18 22:51:20 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
