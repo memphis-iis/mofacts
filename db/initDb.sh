171f26e6a9209735a567fcb66f141ebad24f29b2
wscarter <wescarterdev@fastmail.com>
2021-02-03 16:12:58 -0600

    db to host

--- db/initDb.sh
+++ db/initDb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#sudo apt-get install postgresql
+sudo apt-get install postgresql
 sudo -u postgres psql -c "CREATE USER mofacts WITH PASSWORD 'test101';"
 sudo -u postgres createdb mofacts
 sudo adduser --disabled-password --gecos "" mofacts
-sudo echo "local   mofacts         mofacts                                 md5" >> /etc/postgresql/10/main/pg_hba.conf
-
+sudo echo "host    all             all             10.0.0.0/16             md5" >> /etc/postgresql/10/main/pg_hba.conf
+sudo echo "listen_addresses = '*'" >> /etc/postgresql/10/main/postgresql.conf
 sudo -u mofacts psql -f initTables.sql
 sudo apt-get install npm -y
 npm install pg-promise
 sudo -u mofacts which node initFixtures.js
+sudo systemctl restart postgresql
