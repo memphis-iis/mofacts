ef93ff072bfb32b5524442780da3ac6b2d7b7b9e
wscarter <wescarterdev@fastmail.com>
2021-02-05 12:41:40 -0600

    make db owner mofacts

--- db/initDb.sh
+++ db/initDb.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install postgresql
 sudo -u postgres psql -c "CREATE USER mofacts WITH PASSWORD 'test101';"
-sudo -u postgres createdb mofacts
+sudo -u postgres createdb mofacts --owner=mofacts
 sudo adduser --disabled-password --gecos "" mofacts
 sudo echo "host    all             all             10.0.0.0/16             md5" >> /etc/postgresql/10/main/pg_hba.conf
 sudo echo "listen_addresses = '*'" >> /etc/postgresql/10/main/postgresql.conf
