cec63651e3dac9340f313655003b6e62df59005a
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-08 12:55:12 -0600

    fix typo that prevented experiments loading properly

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"ad57c8d7493175a888ea2bd4ce1a309fc50101bf",
-"buildTimestamp":"Mon Jan 18 22:51:20 CST 2021",
+"commitID":"2454ae711d26b3c0fd12ace7c316b3c823ac0db1",
+"buildTimestamp":"Mon Feb  8 12:48:29 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
