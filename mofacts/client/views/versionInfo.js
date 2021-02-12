461e314809efc7d31cc91efc645a4ebf96be322f
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-12 17:24:11 -0600

    sort templates in chronological order in content gen

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"12d8f522872e87f41be6141107f8944428919b75",
-"buildTimestamp":"Fri Feb 12 14:54:56 CST 2021",
+"commitID":"5de5560a6cff2c4b7d09c87344b6ec2de2e7d905",
+"buildTimestamp":"Fri Feb 12 16:16:13 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
