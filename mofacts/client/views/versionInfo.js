a9e4b801b59ab669670d890dd9aeaec427a29673
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-14 18:02:53 -0600

    fix dialogue type selector not loading from db correctly due to typo

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"4af81240c4d2a7b7e1aa64d3c859842c1b34f0b3",
-"buildTimestamp":"Fri Feb 12 17:24:18 CST 2021",
+"commitID":"a34463f65c8f20478f61acd2d5c2a40a603a4848",
+"buildTimestamp":"Sun Feb 14 01:12:29 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
