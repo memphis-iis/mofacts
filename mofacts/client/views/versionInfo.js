66c2c8204e150e725776359778b37051df05f596
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-15 16:22:21 -0600

    fix bug where resume from error report inside of dialogue loop causes exception

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"a34463f65c8f20478f61acd2d5c2a40a603a4848",
-"buildTimestamp":"Sun Feb 14 01:12:29 CST 2021",
+"commitID":"4ddcdee88e94562cedc39cdd218812dbc744267b",
+"buildTimestamp":"Mon Feb 15 12:01:16 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
