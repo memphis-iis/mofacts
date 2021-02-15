ea2a23ab03642ffa3d2bf3707b57cc423570d732
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-14 18:02:53 -0600

    fix dialogue type selector not loading from db correctly due to typo

--- mofacts/client/views/home/profileDialogueToggles.js
+++ mofacts/client/views/home/profileDialogueToggles.js
@@ -3,20 +3,22 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 const _state = new ReactiveDict('dialogueSelectState');
 const _availableDialogueTypes = ["simple", "refutational", "dialogue"];
 
-const _randomizeSelectedDialogueType = () => {
+const _randomizeSelectedDialogueType = async () => {
   const rIdx = Math.floor(Math.random() * Math.floor(_availableDialogueTypes.length));
   _state.set("randomSelectedDialogueType", _availableDialogueTypes[rIdx]);
   let selectedDialogueType = _state.get("randomSelectedDialogueType");
   _state.set("selectedDialogueType", selectedDialogueType);
-  Meteor.call('setUserDialogueTypeState',Meteor.userId(),selectedDialogueType);
+  await Meteor.call('setUserDialogueTypeState',Meteor.userId(),selectedDialogueType);
 }
 
-Template.profileDialogueToggles.rendered = function() {
-  let dialogueToggleState = Meteor.user().dialogueToggleState;
-  if(dialogueToggleState){
-    _state.set("randomSelectedDialogueType", dialogueToggleState);
-    _state.set("selectedDialogueType", dialogueToggleState);
+Template.profileDialogueToggles.rendered = async function() {
+  let dialogueTypeState = Meteor.user().dialogueTypeState;
+  if(dialogueTypeState){
+    console.log("profileDialogueToggles,state exists:",dialogueTypeState);
+    _state.set("randomSelectedDialogueType", dialogueTypeState);
+    _state.set("selectedDialogueType", dialogueTypeState);
   }else{
+    console.log("profileDialogueToggles,state doesn't exist:",dialogueTypeState);
     _randomizeSelectedDialogueType();
   }
 }
