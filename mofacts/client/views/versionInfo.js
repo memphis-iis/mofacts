e1736ab13c8f49aa20b8f8f2a05e47e338926df0
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-17 16:01:35 -0600

    more streamlining of signinsouthwest page load

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"20c123e79db08cbcdc3c8f8c60f3a6c17bad049e",
-"buildTimestamp":"Tue Feb 16 21:13:32 CST 2021",
+"commitID":"729e8f757041fdcaa46ec73363e94c3b272a2b7b",
+"buildTimestamp":"Wed Feb 17 15:47:01 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
