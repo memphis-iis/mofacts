b66b3bcd00db15569111bc2b65f9a8eea4b8fc18
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-18 14:02:42 -0600

    fix typo in handling of edge case in content gen where primary stim is deleted but not all alternate displays are

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"41f77cef09d6d3d300301514a3d4b9cbcda93c3d",
-"buildTimestamp":"Wed Feb 17 16:01:43 CST 2021",
+"commitID":"d23746310a5668c0f0cb0c35fe1f9accbd4be81c",
+"buildTimestamp":"Wed Feb 17 16:42:01 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
