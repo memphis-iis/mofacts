d4c6f03eecdecda08883e36b1888dbbf3e1dbebe
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-19 11:03:14 -0600

    shorten edit dist hit on correct answer feedback so correctprompt times arent too short

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"8ad392f0d187125b02b915158d6c2f1c5207e003",
-"buildTimestamp":"Thu Feb 18 14:03:14 CST 2021",
+"commitID":"3d06b5abac48927f7acb8e4272f7b454393439d9",
+"buildTimestamp":"Fri Feb 19 09:23:48 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
