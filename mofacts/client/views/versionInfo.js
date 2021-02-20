36b4d49692972fa42cf710bb94e16ab42b2a8120
Andrew Tackett <andrew.tackett@gmail.com>
2021-02-20 09:46:48 -0600

    add disclaimer that load time is long on signin

--- mofacts/client/views/versionInfo.js
+++ mofacts/client/views/versionInfo.js
@@ -1,8 +1,8 @@
 //NOTE: This file will be out of date on any machine except the one that last created the deployment tarball
 Session.set("versionInfo",
 {
-"commitID":"d4c6f03eecdecda08883e36b1888dbbf3e1dbebe",
-"buildTimestamp":"Fri Feb 19 15:10:33 CST 2021",
+"commitID":"4ea654345b1df9976a8427dce68b4715195caf3c",
+"buildTimestamp":"Fri Feb 19 17:11:09 CST 2021",
 "buildersUsername":"Andrew Tackett",
 "branch":"master",
 "repoURL":"git@github.com:memphis-iis/mofacts-ies.git"
