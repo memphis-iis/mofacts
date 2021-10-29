c911a22068dd0ab789dffa8acc693360237e58bc
JRustyHaner <jrhaner@memphis.edu>
2021-10-29 08:44:55 -0500

    Asset uploader thing

---
+++ mofacts/client/views/experimentSetup/assetUpload.js
@@ -0,0 +1,49 @@
+import { Template }    from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { FilesCollection } from 'meteor/ostrio:files';
+import { Route53RecoveryCluster, Route53Resolver } from '../../../node_modules/aws-sdk/index';
+
+Template.assetUpload.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
+});
+
+Template.assetUpload.helpers({
+  currentUpload() {
+    return Template.instance().currentUpload.get();
+  },
+  'isUserInRole': function(userId, role) {
+    return Roles.userIsInRole(userId, role);
+  },
+  assetLink: () => Session.get('assetLink'),
+});
+
+Template.assetUpload.events({
+  'change #fileInput'(e, template) {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // multiple files were selected
+      const upload = DynamicAssets.insert({
+        file: e.currentTarget.files[0],
+        chunkSize: 'dynamic'
+      }, false);
+
+      upload.on('start', function () {
+        template.currentUpload.set(this);
+      });
+
+      upload.on('end', function (error, fileObj) {
+        if (error) {
+          alert(`Error during upload: ${error}`);
+        } else {
+          alert(`File "${fileObj.name}" successfully uploaded`);
+          console.log(DynamicAssets.link(fileObj));
+          Session.set('assetLink',DynamicAssets.link(fileObj));
+        }
+        template.currentUpload.set(false);
+      });
+
+      upload.start();
+
+    }
+  }
+});
--- mofacts/common/Collections.js
+++ mofacts/common/Collections.js
@@ -14,3 +14,17 @@ LoginTimes = new Mongo.Collection('loginTimes');
 UtlQueryTimes = new Mongo.Collection('utlQueryTimes');
 DynamicConfig = new Mongo.Collection('dynamicConfig');
 UserProfileData = new Mongo.Collection('userProfileData');
+
+//Init DynamicAssets Collection
+DynamicAssets = new FilesCollection({
+    collectionName: 'Assets',
+    storagePath: process.env.HOME + '/dynamic-assets',
+    allowClientCode: false, // Disallow remove files from Client
+    onBeforeUpload(file) {
+      // Allow upload files under 10MB, and only in png/jpg/jpeg formats
+      if (file.size <= 10485760 && /png|jpg|jpeg|bmp|wav|mp3|mp4|mov|mpg|mpeg|tif|webm|flac/i.test(file.extension)) {
+        return true;
+      }
+      return 'Please upload image, audio, or video fi with size equal or less than 10MB';
+    }
+  });
