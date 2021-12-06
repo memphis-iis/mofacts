cb064c78bad28b7295e5c969e3ddc0ff7270e0cc
Rhodes White <rawhite2@memphis.edu>
2021-12-06 15:35:34 -0600

    fixed error with refutational feedback not showing. Disabled countdown timer as it is breaking refutational feedback

--- mofacts/server/methods.js
+++ mofacts/server/methods.js
@@ -1808,7 +1808,7 @@ Meteor.startup(async function() {
 
     getSimpleFeedbackForAnswer: function(userAnswer, correctAnswer) {
       // eslint-disable-next-line new-cap
-      const result = DefinitionalFeedback.GenerateFeedback(userAnswer, correctAnswer);
+      const result = ElaboratedFeedback.GenerateFeedback(userAnswer, correctAnswer);
       console.log('result: ' + JSON.stringify(result));
       return result;
     },
