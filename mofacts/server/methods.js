e11da178a011757cf183e8d4fa0f0ea7f15170d2
Rhodes White <rawhite2@memphis.edu>
2022-01-24 15:50:40 -0600

    fixed querey that quintupled expected return

--- mofacts/server/methods.js
+++ mofacts/server/methods.js
@@ -1147,8 +1147,7 @@ async function getStudentPerformanceByIdAndTDFId(userId, TDFid,hintLevel=null,re
                SUM(s.totalPracticeDuration) AS totalPracticeDuration, \
                COUNT(CASE WHEN (s.priorIncorrect = 1 AND s.priorCorrect = 0) OR (s.priorIncorrect = 0 AND s.priorCorrect = 1) THEN 1 END) AS stimsIntroduced \
                FROM (SELECT * from componentState WHERE userId=$1 AND TDFId=$2 AND componentType =\'stimulus\' \ AND showitem = true  ' + hintLevelAddendunm + limitAddendum + ') AS s \
-               INNER JOIN item AS i ON i.stimulusKC = s.KCId \
-               INNER JOIN tdf AS t ON t.stimuliSetId = i.stimuliSetId';
+               INNER JOIN item AS i ON i.stimulusKC = s.KCId';
   const perfRet = await db.oneOrNone(query, [userId, TDFid]);
   const query2 = 'SELECT COUNT(DISTINCT s.ItemId) AS stimsSeen \
                   FROM history AS s \
