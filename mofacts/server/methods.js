ae7145f807e8424f3d1bf736604ee9f728af60e7
JRustyHaner <jrhaner@memphis.edu>
2022-01-25 11:50:20 -0600

    edit

--- mofacts/server/methods.js
+++ mofacts/server/methods.js
@@ -2180,7 +2180,6 @@ Meteor.startup(async function() {
           if (lessonName.length < 1) {
             results.result = false;
             results.errmsg = 'TDF has no lessonname - it cannot be valid';
-
             return results;
           }
           const stimFileName = json.tutor.setspec.stimulusfile ? json.tutor.setspec.stimulusfile : 'INVALID';
