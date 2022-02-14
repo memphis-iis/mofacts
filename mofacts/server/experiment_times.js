c1b9258b38824572de3f1c2691a554ae153be07d
JRustyHaner <jrhaner@memphis.edu>
2022-02-14 10:31:20 -0600

    Issue 634: added check for rendundant outputs

--- mofacts/server/experiment_times.js
+++ mofacts/server/experiment_times.js
@@ -86,7 +86,10 @@ async function createExperimentExport(expName) {
   const listOfDynamicStimTags = await getListOfStimTags(expName);
   const listOfDynamicStimTagsWithColumnNames = [];
   for (const tag of listOfDynamicStimTags) {
-    listOfDynamicStimTagsWithColumnNames.push('CF (' + tag + ')');
+    let renamedField = 'CF (' + tag + ')';
+    if(FIELDSDS.indexOf(renamedField) !== -1){
+      listOfDynamicStimTagsWithColumnNames.push(renamedField);
+    }
   }
 
   FIELDSDS = FIELDSDS.concat(listOfDynamicStimTagsWithColumnNames);
