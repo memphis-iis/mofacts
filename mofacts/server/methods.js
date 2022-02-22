49a257c7cf6402c06186f7bbc6746852d1d6deaf
JRustyHaner <jrhaner@memphis.edu>
2022-02-22 11:10:10 -0600

    syllables happen on upload

--- mofacts/server/methods.js
+++ mofacts/server/methods.js
@@ -2261,6 +2261,25 @@ Meteor.startup(async function() {
               try {
                 const rec = {'fileName': filename, 'tdfs': json, 'ownerId': ownerId, 'source': 'upload'};
                 await upsertTDFFile(filename, rec, ownerId);
+                cachedSyllables = StimSyllables.findOne({filename: stimuliSetId});
+                const stims = await getStimuliSetById(stimuliSetId);
+                let allAnswers = new Set();
+                for (const stim of stims) {
+                  const responseParts = stim.correctResponse.toLowerCase().split(';');
+                  const answerArray = responseParts.filter(function(entry) {
+                    return entry.indexOf('incorrect') == -1;
+                  });
+                  if (answerArray.length > 0) {
+                    const singularAnswer = answerArray[0].split('~')[0];
+                    allAnswers.add(singularAnswer);
+                  }
+                }
+              
+                allAnswers = Array.from(allAnswers);
+                if (!cachedSyllables) {
+                  console.log('no cached syllables for this stim, calling server method to create them');
+                  Meteor.call('updateStimSyllableCache', stimuliSetId, allAnswers)
+                }
                 results.result = true;
               } catch (err) {
                 results.result=false;
