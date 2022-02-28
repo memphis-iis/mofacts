227e152f7882872153f1a9ff047a82654c0801cd
Rhodes White <rawhite2@memphis.edu>
2022-02-28 14:05:35 -0600

    fixed db issues added testing buttons

--- mofacts/client/views/experiment/unitEngine.js
+++ mofacts/client/views/experiment/unitEngine.js
@@ -724,11 +724,11 @@ function modelUnitEngine() {
           //Detect Hint Levels
           if (!this.cachedSyllables.data || !this.cachedSyllables.data[answerText]) {
             hintLevelIndex = 1;
-            console.log('no cached syllables for: ' + currentStimuliSetId + '|' + answerText + '. hintlevel index is 1.');
+            console.log('no cached syllables for: ' + currentStimuliSetId + ' | ' + answerText + '. hintlevel index is 1.');
           } else {
             const stimSyllableData = this.cachedSyllables.data[answerText];
             hintLevelIndex = stimSyllableData.count;
-            console.log('syllables detected for: ' + currentStimuliSetId + '|' + answerText + '. hintlevel index is ' + hintLevelIndex);
+            console.log('syllables detected for: ' + currentStimuliSetId + ' | ' + answerText + '. hintlevel index is ' + hintLevelIndex);
           }
           parms = this.calculateSingleProb(i, j, 0, count);
           tdfDebugLog.push(parms.debugLog);
