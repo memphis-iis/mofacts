2c01dc85912349ebe1b2c4e78e7fa4e9418a9b7e
Rhodes White <rawhite2@memphis.edu>
2022-05-19 14:32:53 -0500

    fixed data not present in data download

--- mofacts/common/Definitions.js
+++ mofacts/common/Definitions.js
@@ -38,6 +38,10 @@ export const outputFields = [
   'Student Response Subtype', // qtype
   'Tutor Response Type', // trialType
   'Tutor Response Subtype', // qtype
+  'Hint Level',
+  'Feedback Type',
+  'Feedback Text',
+  'Dialogue History',
   'KC (Default)',
   'KC Category(Default)',
   'KC (Cluster)',
@@ -62,7 +66,5 @@ export const outputFields = [
   'CF (Button Order)', // CF buttonOrder
   'CF (Item Removed)', // item was reported by the user as wrong
   'CF (Note)', // CF note
-  'Feedback Text',
-  'dialogueHistory',
-  'Entry_Point'
+  'Entry Point'
 ];
