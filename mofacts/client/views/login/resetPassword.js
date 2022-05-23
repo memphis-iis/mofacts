546f0550764cc499ef0b3568a7ba5480c3e8f592
James Rusty Haner <jrhaner@memphis.edu>
2022-05-23 10:21:33 -0500

    added password reset function for not Oauth users

---
+++ mofacts/client/views/login/resetPassword.js
@@ -0,0 +1,64 @@
+import { Route53RecoveryCluster } from "../../../node_modules/aws-sdk/index";
+
+Template.resetPassword.onRendered(function() {
+  if (Session.get('loginMode') !== 'experiment') {
+    console.log('password signin, setting login mode');
+    Session.set('loginMode', 'password');
+  }
+});
+
+// //////////////////////////////////////////////////////////////////////////
+// Template Events
+
+Template.resetPassword.events({
+  'click #sendSecret': function(event) {
+      event.preventDefault();
+      email = $('#email').val();
+      Meteor.call('sendPasswordResetEmail', email, function(err, res){
+          alert('If you have an account, an email was sent to you.');
+      });
+      $('#verifySecret').show();
+      $('#sendPasswordEmail').hide();
+  },
+  'click #verifySecretButton': function(event) {
+    event.preventDefault();
+    email = $('#email').val();
+    secret = $('#secret').val();
+    Meteor.call('checkPasswordResetSecret', email, secret, function(err, res){
+        if(res == true){
+            $('#verifySecret').hide();
+            $('#resetPasswordForm').show();
+        } else {
+            alert('Your secret is incorrect.');
+            Router.go('/resetPassword');
+        }
+    });
+
+},
+  'click #resetPasswordButton': function(event) {
+    event.preventDefault();
+    secret = $('#secret').val();
+    resetPassword = $('#password').val();
+    resetPasswordVerify = $('#passwordVerfiy').val();
+    email = $('#email').val();
+    if(resetPassword !== resetPasswordVerify){
+        alert("Passwords do not match.")
+    } else {
+        if(resetPassword.length > 5){
+            Meteor.call('resetPasswordWithSecret', email, secret, resetPassword);
+            Router.go('/signin');
+        }else{
+            alert("Password must be at least 6 characters.");
+        }
+    }
+  },
+});
+
+// //////////////////////////////////////////////////////////////////////////
+// Template Heleprs
+
+Template.resetPassword.helpers({
+  secret: function() {
+    return Session.get('loginMode') === 'experiment';
+  }
+});
