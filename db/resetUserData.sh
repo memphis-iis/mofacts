09eec0b0f61a127276f8aadbe86b98520bdfe92c
Rhodes White <rawhite2@memphis.edu>
2022-09-08 10:45:05 -0500

    fixed wrong feedback

--- db/resetUserData.sh
+++ db/resetUserData.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo -u mofacts psql -c "DROP TABLE IF EXISTS componentstate, globalexperimentstate, history;"
+mongo MoFaCT --eval "db.component_state.drop()";
+mongo MoFaCT --eval "db.global_experiment_state.drop()";
+mongo MoFaCT --eval "db.history.drop()";
 
-#Note we're discarding all error messages here.  If an issue arises, disable this.
-sudo -u mofacts psql -f initTables.sql 2>/dev/null
+mongo MoFaCT --eval "db.createCollection('component_state')";
+mongo MoFaCT --eval "db.createCollection('global_experiment_state')";
+mongo MoFaCT --eval "db.createCollection('history')";
