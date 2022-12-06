ec8e4e0e2bb5dd0ace296afe2f50b7a5f801735b
Rhodes White <rawhite2@memphis.edu>
2022-12-06 10:00:39 -0600

    allow user to define email to send generated content too

--- mofacts/client/views/experimentSetup/contentGeneration.js
+++ mofacts/client/views/experimentSetup/contentGeneration.js
@@ -5,7 +5,9 @@ import {rangeVal} from '../../lib/currentTestingHelpers';
 import {meteorCallAsync} from '../..';
 
 Template.contentGeneration.onRendered(async function() {
-  
+  if(Meteor.user() && Meteor.user().emails && Meteor.user().emails[0].address) {
+    $('#sendToEmail').val(Meteor.user().emails[0].address)
+  }
 });
 
 Template.contentGeneration.events({
@@ -15,15 +17,15 @@ Template.contentGeneration.events({
     const inputTextArray = inputText.split(/\s*###\s*/gm) //split input on ###, with optional whitespace
     const stringArrayJson = JSON.stringify(inputTextArray); 
     const compressionLevel = $('#compressionLevel').val();
+    const userEmail = $('#sendToEmail').val();
     if(compressionLevel > .99 || compressionLevel < .01) {
       alert('Compression level must be between 0.01 and 0.99');
     } else {
-      Meteor.call('generateContent', compressionLevel, stringArrayJson, inputText);
-      alert(`Content generation has begun. An email will be sent too ${Meteor.user().emails[0].address} with your file when it is complete.`);
+      Meteor.call('generateContent', compressionLevel, stringArrayJson, inputText, userEmail);
+      alert(`Content generation has begun. An email will be sent too ${userEmail} with your file when it is complete.`);
     }
   },
 });
 
 Template.contentGeneration.helpers({
-  
 });
