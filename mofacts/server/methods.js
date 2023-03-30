f3c8476f93c26c05bf251f331a0e64a550282fe1
Rhodes White <rawhite2@memphis.edu>
2023-03-30 14:52:55 -0500

    reset totalpracticeduration with clearcurunitprogress

--- mofacts/server/methods.js
+++ mofacts/server/methods.js
@@ -444,7 +444,7 @@ async function getComponentStatesByUserIdTDFIdAndUnitNum(userId, TDFId) {
 }
 
 async function clearCurUnitProgress(userId, TDFId) {
-  ComponentStates.update({userId: userId, TDFId: TDFId}, {$set: {'priorCorrect': 0, 'priorIncorrect': 0}}, {multi: true});
+  ComponentStates.update({userId: userId, TDFId: TDFId}, {$set: {'priorCorrect': 0, 'priorIncorrect': 0, 'totalPracticeDuration': 0}}, {multi: true});
 }
 
 async function setComponentStatesByUserIdTDFIdAndUnitNum(userId, TDFId, componentStates) {
