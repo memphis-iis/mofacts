40d3b41e7518c3a4b628f1e462b398f0cd660b8b
Rhodes White <50426711+MegaGeese@users.noreply.github.com>
2023-04-27 15:22:34 -0500

    update componentstate via minimongo instance

--- mofacts/common/Collections.js
+++ mofacts/common/Collections.js
@@ -40,3 +40,12 @@ DynamicAssets = new FilesCollection({
     return 'Please upload image, audio, or video fi with size equal or less than 10MB';
   }
 });
+
+ComponentStates.allow({
+  update: function(userId, doc, fieldNames, modifier) {
+    return userId === doc.userId;
+  },
+  insert: function(userId, doc) {
+    return userId === doc.userId;
+  }
+})
