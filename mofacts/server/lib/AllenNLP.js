cc44097391480079a560b0a80a2606524faedcf6
James Haner <jrhaner@memphis.edu>
2023-07-12 10:31:35 -0500

    changes to allow custom authoring

--- mofacts/server/lib/AllenNLP.js
+++ mofacts/server/lib/AllenNLP.js
@@ -245,7 +245,7 @@ function EntailmentRequest$reflection() {
   return (0, _Reflection.record_type)("AllenNLP.EntailmentRequest", [], EntailmentRequest, () => [["hypothesis", _Reflection.string_type], ["premise", _Reflection.string_type]]);
 }
 
-const endpoints = new Endpoints("https://allennlp/predict/semantic-role-labeling", "https://allennlp/predict/coreference-resolution", "https://allennlp/predict/dependency-parsing", "https://spacy:8003/sents", "https://spacy:8003/predict/textual-entailment");
+const endpoints = new Endpoints("https://allennlp:8002/predict/semantic-role-labeling", "https://allennlp:8002/predict/coreference-resolution", "https://allennlp:8002/predict/dependency-parsing", "https://spacy:8001/sents", "https://allennlp:8002/predict/textual-entailment");
 exports.endpoints = endpoints;
 
 function GetCoreference(input$$1) {
