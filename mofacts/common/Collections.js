5b49418192e5cb6c7d8129567d90e7d8df951d40
James Haner <jrhaner@memphis.edu>
2023-08-04 08:16:57 -0500

    change wording for uploader to zips only

--- mofacts/common/Collections.js
+++ mofacts/common/Collections.js
@@ -35,7 +35,7 @@ DynamicAssets = new FilesCollection({
   allowClientCode: true, // Disallow remove files from Client
   onBeforeUpload(file) {
     // Allow upload files under 10MB, and only in png/jpg/jpeg formats
-    if (file.size <= 10485760 && /zip|png|gif|jpg|jpeg|bmp|wav|mp3|mp4|mov|mpg|mpeg|tif|webm|flac/i.test(file.extension)) {
+    if (file.size <= 10485760 && /zip/i.test(file.extension)) {
       return true;
     }
     return 'Please upload image, audio, or video fi with size equal or less than 10MB';
