89408a1c7cc3bc195c69f7316cb15f9c7fc2372b
James Haner <jrhaner@memphis.edu>
2023-10-17 07:51:46 -0500

    revert

--- mofacts/client/views/experiment/card.js
+++ mofacts/client/views/experiment/card.js
@@ -1502,7 +1502,7 @@ async function showUserFeedback(isCorrect, feedbackMessage, afterAnswerFeedbackC
         }
         //if the displayOnlyCorrectAnswerAsFeedbackOverride is set to true, then we will display the correct answer in feedbackOverride div
         if (Session.get('curTdfUISettings').displayCorrectAnswerInCenter) {
-          const correctAnswer = Session.get('currentExperimentState').originalAnswer;
+          const correctAnswer = Answers.getDisplayAnswerText(Session.get('currentExperimentState').currentAnswer);
           $('#feedbackOverride').html(correctAnswer).attr("hidden",false).show();
         }
         
