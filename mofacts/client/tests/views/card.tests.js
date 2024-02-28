c3256b57eb61806a11de82ba09dddee87ccb460e
August White <August.White@memphis.edu>
2024-02-28 10:06:42 -0600

    adding integration testing

---
+++ mofacts/client/tests/views/card.tests.js
@@ -0,0 +1,53 @@
+import { Meteor } from 'meteor/meteor';
+import { Factory } from 'meteor/dburles:factory';
+import { Random } from 'meteor/random';
+import chai from 'chai';
+import StubCollections from 'meteor/hwillson:stub-collections';
+import { Template } from 'meteor/templating';
+import $ from 'jquery';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import sinon from 'sinon';
+
+import { withRenderedTemplate } from '../../test-helpers.js';
+import '../lists-show-page.js';
+
+import { Todos } from '../../../api/todos/todos.js';
+import { Lists } from '../../../api/lists/lists.js';
+
+describe('Lists_show_page', function () {
+    const listId = Random.id();
+
+    beforeEach(function () {
+        StubCollections.stub([Todos, Lists]);
+        Template.registerHelper('_', key => key);
+        sinon.stub(FlowRouter, 'getParam').returns(listId);
+        sinon.stub(Meteor, 'subscribe').returns.({
+            subscriptionId: 0,
+            ready: () => true,
+        });
+    });
+
+    afterEach(function () {
+        StubCollections.restore();
+        Template.deregisterHelper('_');
+        FlowRouter.getParam.restore();
+        Meteor.subscribe.restore();
+    });
+
+    it('renders correctly with simple data', function () {
+        Factory.create('list', { _id: listId });
+        const timestamp = new Date();
+        const todos = [...Array(3).keys()].forEach(i => Factory.create('todo', {
+            listId,
+            createdAt: new Date(timestamp - (3 - i)),
+        }));
+
+        withRenderedTemplate('Lists_show_page', {}, el => {
+            const todosText = todos.map(t => t.text).reverse();
+            const renderedText = $(el).find('.list-items input[type=text]')
+            .map((i, e) => $(e).val())
+            .toArray();
+            chai.assert.deepEqual(renderedText, todosText);
+        });
+    });
+});
