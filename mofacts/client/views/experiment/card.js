acd909c9bd64b372eedb0f2825afde6f80ab62f1
James "Rusty" Haner <46696077+JRustyHaner@users.noreply.github.com>
2025-05-19 13:43:04 -0500

    removed incorrect button checks

--- mofacts/client/views/experiment/card.js
+++ mofacts/client/views/experiment/card.js
@@ -1181,7 +1181,6 @@ function setUpButtonTrial() {
   let buttonChoices = [];
   const buttonOrder = currUnit.buttonorder ? currUnit.buttonorder.trim().toLowerCase() : "";
   const buttonOptions = currUnit.buttonOptions ? currUnit.buttonOptions.trim() : "";
-  let correctButtonPopulated = null;
 
   if (buttonOptions) {
     if(typeof buttonOptions == "object"){
@@ -1190,20 +1189,14 @@ function setUpButtonTrial() {
     else{
       buttonChoices = buttonOptions.split(',');
     }
-    correctButtonPopulated = true;
-    console.log('buttonChoices==buttonOptions', buttonChoices);
   } else {
     const currentFalseResponses = getCurrentFalseResponses();
     for (const falseResponse of currentFalseResponses) {
       buttonChoices.push(falseResponse);
-      correctButtonPopulated = false;
     }
     console.log('buttonChoices==falseresponses and correct answer', buttonChoices);
   }
-  if (correctButtonPopulated == null) {
-    console.log('No correct button');
-    throw new Error('Bad TDF/Stim file - no buttonOptions and no false responses');
-  }
+
 
   const currentAnswer = Session.get('currentExperimentState').originalAnswer;
   const correctAnswer = Answers.getDisplayAnswerText(currentAnswer);
